349feff8d4ad17ceda23a3a24bb53b15c1855f3e
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-11-12 14:02:34 -0500

    MAT-3509: Adding new page for Editing a measure

---
+++ src/components/ViewEditMeasure.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ViewEditMeasure() {
+  return (
+    <>
+      <span>Add additional Details</span>
+    </>
+  );
+}
--- src/madie-madie-measure.tsx
+++ src/madie-madie-measure.tsx
@@ -4,6 +4,7 @@ import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 import MeasureLanding from "./components/MeasureLanding";
 import CreateNewMeasure from "./components/CreateNewMeasure";
+import ViewEditMeasure from "./components/ViewEditMeasure";
 
 const lifecycles = singleSpaReact({
   React,
@@ -17,5 +18,6 @@ const lifecycles = singleSpaReact({
 
 export const MadieMeasure: FC = MeasureLanding;
 export const NewMeasure: FC = CreateNewMeasure;
+export const EditMeasure: FC = ViewEditMeasure;
 
 export const { bootstrap, mount, unmount } = lifecycles;
