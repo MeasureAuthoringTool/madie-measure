3877c86441b465bfa8c000588aa3a29d9f0359a0
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-11-15 16:33:25 -0500

    MAT-3509: fixed some things that would allow to work with single-spa-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { MadieMeasure } from "./madie-madie-measure";
 import CreateNewMeasure from "./components/CreateNewMeasure";
 
