b6212ab4c7ffeae5812a1282932260353b93cd7b
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2021-11-16 11:13:16 -0500

    Add basic measure list display
    - Measure service url configured in `src/components/Config.ts`

---
+++ src/components/MeasureList.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import tw from "twin.macro";
+import { Measure } from "./MeasureLanding";
+
+export default function MeasureList(props: { measureList: Measure[] }) {
+  return (
+    <div data-testid="measure-list">
+      <div tw="flex flex-col">
+        <div tw="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div tw="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div tw="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table tw="min-w-full divide-y divide-gray-200">
+                <thead tw="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      tw="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      tw="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Model
+                    </th>
+                    <th
+                      scope="col"
+                      tw="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      State
+                    </th>
+                    <th
+                      scope="col"
+                      tw="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Version
+                    </th>
+                    <th
+                      scope="col"
+                      tw="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Revision
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {props.measureList?.map((measure, measureIdx) => (
+                    <tr tw="bg-white">
+                      <td tw="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        <button
+                          type="button"
+                          onClick={() => (window.location.href = "#")}
+                          data-testid={`measure-button-${measureIdx}`}
+                        >
+                          {measure.name}
+                        </button>
+                      </td>
+                      <td tw="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {measure.model}
+                      </td>
+                      <td tw="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {measure.state}
+                      </td>
+                      <td tw="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {measure.version}
+                      </td>
+                      <td tw="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {measure.revisionNumber}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
