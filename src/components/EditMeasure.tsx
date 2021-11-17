9fd3919a61ac382897131ec7846b2b65f948694a
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-11-17 14:51:18 -0500

    MAT-3507: Fixed bad name in cmoponent

--- src/components/EditMeasure.tsx
+++ src/components/EditMeasure.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import tw from "twin.macro";
 import { TextInput, Label, Button } from "@madie/madie-components";
 
-export default function CreateNewMeasure() {
+export default function EditMeasure() {
   const { id } = useParams();
 
   return <div tw="w-1/4 mx-auto my-8">Edit a measure {id}</div>;
