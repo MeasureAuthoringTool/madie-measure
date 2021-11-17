7c0c4ab800088eb4ff060cae7419c2dfcff9d553
Gregory P. Akins <gregory.akins@semanticbits.com>
2021-11-17 13:24:54 -0500

    MAT-3507: Moved routing into the app and started a EditMeasure 'stub'

---
+++ src/components/EditMeasure.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import tw from "twin.macro";
+import { TextInput, Label, Button } from "@madie/madie-components";
+
+export default function CreateNewMeasure() {
+  const { id } = useParams();
+
+  return <div tw="w-1/4 mx-auto my-8">Edit a measure {id}</div>;
+}
