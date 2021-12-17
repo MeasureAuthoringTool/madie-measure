76b819a83e3542af33b12e4d3dd65f34bc4c7661
Ashok Dongare <ashok.dongare@semanticbits.com>
2021-12-17 15:42:08 -0500

    MAT-3536 Save CQL to Measure Data Model

--- src/components/MeasureEditor/MeasureEditor.tsx
+++ src/components/MeasureEditor/MeasureEditor.tsx
@@ -1,15 +1,39 @@
-import React, { SetStateAction, Dispatch } from "react";
+import React, { SetStateAction, Dispatch, useState } from "react";
 import "twin.macro";
 import "styled-components/macro";
 import { MadieEditor } from "@madie/madie-editor";
 import { Button } from "@madie/madie-components";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
+import useCurrentMeasure from "../EditMeasure/useCurrentMeasure";
+import Measure from "../../models/Measure";
+import useMeasureServiceApi from "../../api/useMeasureServiceApi";
 
 const MeasureEditor = () => {
+  const { measure, setMeasure } = useCurrentMeasure();
   const [editorVal, setEditorVal]: [string, Dispatch<SetStateAction<string>>] =
-    useLocalStorage("editorVal", "");
+    useState(measure.cql);
+  const measureServiceApi = useMeasureServiceApi();
+
+  const updateMeasureCql = () => {
+    if (editorVal !== measure.cql) {
+      const newMeasure: Measure = { ...measure, cql: editorVal };
+      measureServiceApi
+        .updateMeasure(newMeasure)
+        .then(() => {
+          setMeasure(newMeasure);
+        })
+        .catch((reason) => {
+          console.error(reason);
+          throw new Error(reason.message);
+        });
+    }
+  };
+
   const handleMadieEditorValue = (val: string) => {
     setEditorVal(val);
+  };
+
+  const resetCql = (): void => {
+    setEditorVal(measure.cql);
   };
 
   const editorProps = {
@@ -20,13 +44,21 @@ const MeasureEditor = () => {
   return (
     <>
       <MadieEditor {...editorProps} />
-      <div tw="mt-2" data-testid="measure-editor-actions">
-        <Button buttonSize="md" buttonTitle="Save" variant="primary" />
+      <div tw="mt-2 ml-2 mb-5" data-testid="measure-editor-actions">
+        <Button
+          buttonSize="md"
+          buttonTitle="Save"
+          variant="primary"
+          onClick={() => updateMeasureCql()}
+          data-testid="save_cql_btn"
+        />
         <Button
           tw="ml-2"
           buttonSize="md"
           buttonTitle="Cancel"
           variant="secondary"
+          onClick={() => resetCql()}
+          data-testid="reset_cql_btn"
         />
       </div>
     </>
