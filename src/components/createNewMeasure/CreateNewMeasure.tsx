693537dca39fb900171c254a73cc7a63e280bca1
Ashok Dongare <ashok.dongare@semanticbits.com>
2021-12-22 20:07:16 -0500

    MAT-3520: Polished UI lil bit

--- src/components/createNewMeasure/CreateNewMeasure.tsx
+++ src/components/createNewMeasure/CreateNewMeasure.tsx
@@ -10,6 +10,8 @@ import { MeasureSchemaValidator } from "../../models/MeasureSchemaValidator";
 import { getServiceConfig, ServiceConfig } from "../config/Config";
 
 const ErrorAlert = tw.div`bg-red-200 rounded-lg py-3 px-3 text-red-900 mb-3`;
+const FormRow = tw.div`mt-3`;
+
 const CreateNewMeasure = () => {
   const history = useHistory();
   const [serverError, setServerError] = useState(undefined);
@@ -60,46 +62,48 @@ const CreateNewMeasure = () => {
         data-testid="create-new-measure-form"
         onSubmit={formik.handleSubmit}
       >
-        <TextInput
-          type="text"
-          id="measureName"
-          {...formik.getFieldProps("measureName")}
-          placeholder="Enter a Measure Name"
-          data-testid="measure-name-text-field"
-        >
-          <Label htmlFor="measureName" text="Measure Name" />
-          {formikErrorHandler("measureName", true)}
-        </TextInput>
-
-        <TextInput
-          type="text"
-          id="cqlLibraryName"
-          {...formik.getFieldProps("cqlLibraryName")}
-          placeholder="Enter CQL Library Name"
-          data-testid="cql-library-name-name"
-        >
-          <Label htmlFor="cqlLibraryName" text="Measure CQL Library Name" />
-          {formikErrorHandler("cqlLibraryName", true)}
-        </TextInput>
-
-        <div tw="mt-4">
+        <FormRow>
+          <TextInput
+            type="text"
+            id="measureName"
+            {...formik.getFieldProps("measureName")}
+            placeholder="Enter a Measure Name"
+            data-testid="measure-name-text-field"
+          >
+            <Label htmlFor="measureName" text="Measure Name" />
+            {formikErrorHandler("measureName", true)}
+          </TextInput>
+        </FormRow>
+        <FormRow>
+          <TextInput
+            type="text"
+            id="cqlLibraryName"
+            {...formik.getFieldProps("cqlLibraryName")}
+            placeholder="Enter CQL Library Name"
+            data-testid="cql-library-name-name"
+          >
+            <Label htmlFor="cqlLibraryName" text="Measure CQL Library Name" />
+            {formikErrorHandler("cqlLibraryName", true)}
+          </TextInput>
+        </FormRow>
+        <FormRow>
           <Button
             buttonTitle="Create Measure"
             type="submit"
-            tw="mr-4"
+            tw="mr-3"
             data-testid="create-new-measure-save-button"
             disabled={!(formik.isValid && formik.dirty)}
           />
           <Button
             buttonTitle="Cancel"
-            tw="mr-4"
             type="button"
+            variant="white"
             onClick={() => {
               history.push("/measure");
             }}
             data-testid="create-new-measure-cancel-button"
           />
-        </div>
+        </FormRow>
       </form>
     </div>
   );
