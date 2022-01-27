7bff0c16f1639d2d57617474633a4d81df3bf5ef
Nate Moraca <nate.moraca@semanticbits.com>
2022-01-26 21:08:50 -0500

    first pass at my measures; ref #MAT-3813

--- src/components/newMeasure/NewMeasure.test.tsx
+++ src/components/newMeasure/NewMeasure.test.tsx
@@ -2,10 +2,53 @@ import "@testing-library/jest-dom";
 // NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
 
 import * as React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import NewMeasure from "./NewMeasure";
+import axios from "axios";
+import { MeasureScoring } from "../../models/MeasureScoring";
+import { Model } from "../../models/Model";
 
-test("shows the children when the checkbox is checked", () => {
-  render(<NewMeasure />);
-  expect(screen.getByTestId("create-new-measure-button")).toBeTruthy();
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+jest.mock("../config/Config", () => ({
+  getServiceConfig: jest.fn(() =>
+    Promise.resolve({
+      measureService: {
+        baseUrl: "example-service-url",
+      },
+    })
+  ),
+}));
+
+jest.mock("../../hooks/useOktaTokens", () =>
+  jest.fn(() => ({
+    getAccessToken: () => "test.jwt",
+  }))
+);
+
+describe("Measure Page", () => {
+  test("shows the children when the checkbox is checked", async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: [
+        {
+          id: "ab123",
+          measureHumanReadableId: "ab123",
+          measureName: "TestMeasure1",
+          model: Model.QICORE,
+          measureScoring: MeasureScoring.COHORT,
+          measureState: "NA",
+          version: 1.2,
+          revisionNumber: 12,
+        },
+      ],
+    });
+    render(<NewMeasure />);
+    expect(screen.getByTestId("create-new-measure-button")).toBeTruthy();
+    // await waitFor(() => {
+    //   expect(screen.getByText("TestMeasure1")).toBeInTheDocument();
+    // });
+    // const measure1 = await screen.findByText("TestMeasure1");
+    // expect(measure1).toBeInTheDocument();
+  });
 });
--- src/components/newMeasure/NewMeasure.tsx
+++ src/components/newMeasure/NewMeasure.tsx
@@ -1,54 +1,98 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "twin.macro";
 import "styled-components/macro";
 import { Button } from "@madie/madie-components";
 import { useHistory } from "react-router-dom";
 import MeasureList from "../measureList/MeasureList";
 import Measure from "../../models/Measure";
+import * as _ from "lodash";
 
 import { getServiceConfig, ServiceConfig } from "../config/Config";
 import axios from "axios";
 import useOktaTokens from "../../hooks/useOktaTokens";
+import { Divider, InputAdornment, Tab, Tabs, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function NewMeasure() {
   const history = useHistory();
   const [measureList, setMeasureList] = useState<Measure[]>([]);
   const [serviceConfig, setServiceConfig] = useState<ServiceConfig>();
   const [serviceConfigErr, setServiceConfigErr] = useState<string>();
+  const [activeTab, setActiveTab] = useState(0);
   const { getAccessToken } = useOktaTokens();
 
-  if (!serviceConfig && !serviceConfigErr) {
-    (async () => {
-      const config: ServiceConfig = await getServiceConfig();
-      setServiceConfig(config);
+  useEffect(() => {
+    getServiceConfig()
+      .then((serviceConfig) => setServiceConfig(serviceConfig))
+      .catch(() =>
+        setServiceConfigErr(
+          "Unable to load page, please contact the site administration"
+        )
+      );
+  }, []);
+
+  useEffect(() => {
+    if (serviceConfig) {
+      setMeasureList(() => []);
       axios
-        .get(config?.measureService?.baseUrl + "/measures", {
+        .get<Measure[]>(serviceConfig?.measureService?.baseUrl + "/measures", {
           headers: {
             Authorization: `Bearer ${getAccessToken()}`,
           },
+          params: {
+            currentUser: activeTab === 0,
+          },
         })
         .then((response) => {
-          setMeasureList(response.data as Array<Measure>);
-        })
-        .catch((err) => {
-          setServiceConfigErr(
-            "Unable to load page, please contact the site administration"
+          setMeasureList(() =>
+            _.orderBy(response.data, ["lastModifiedAt"], ["desc"])
           );
         });
-    })();
-  }
+    }
+  }, [serviceConfig, activeTab, getAccessToken]);
+
+  const handleTabChange = (event, nextTab) => {
+    setActiveTab(nextTab);
+  };
+
   return (
-    <>
-      <span>Welcome </span>
-      <Button
-        buttonTitle="New Measure"
-        tw="mr-4"
-        onClick={() => history.push("/measure/create")}
-        data-testid="create-new-measure-button"
-      />
-      <div tw="mx-5 my-8">
+    <div tw="mx-12 mt-5">
+      <section tw="flex flex-row my-2">
+        <h1 tw="text-4xl font-light">Measures</h1>
+        <span tw="flex-grow" />
+        <Button
+          buttonTitle="New Measure"
+          tw="h-10"
+          onClick={() => history.push("/measure/create")}
+          data-testid="create-new-measure-button"
+        />
+      </section>
+      <section tw="flex flex-row">
+        <div>
+          <Tabs value={activeTab} onChange={handleTabChange} tw="flex flex-row">
+            <Tab label={`My Measures`} />
+            <Tab label="All Measures" />
+          </Tabs>
+          <Divider />
+        </div>
+        <span tw="flex-grow" />
+        <TextField
+          size="small"
+          placeholder="Search Measures"
+          tw="w-80"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <FontAwesomeIcon icon={faSearch} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </section>
+      <div tw="my-4">
         <MeasureList measureList={measureList} />
       </div>
-    </>
+    </div>
   );
 }
