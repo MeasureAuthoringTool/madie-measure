1ee773e0db2b351c207b33fd6fcaeffae9b4a283
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-03-15 12:32:22 -0500

    resolve code review issue

--- src/components/notfound/NotFound.test.tsx
+++ src/components/notfound/NotFound.test.tsx
@@ -6,19 +6,15 @@ import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 
 describe("NotFound component", () => {
-  it("should render NotFound component", async () => {
-    const { getByTestId, getByText } = render(
-      <MemoryRouter>
-        <NotFound />
-      </MemoryRouter>
-    );
+  it("should render NotFound component",  () => {
+    const { getByTestId, getByText } = render(<NotFound />);
 
     expect(getByTestId("404-page")).toBeInTheDocument();
     expect(getByText("404 - Not Found!")).toBeInTheDocument();
     expect(getByTestId("404-page-link")).toBeInTheDocument();
   });
 
-  it("should render home page after clicking Go Home", async () => {
+  it("should render home page after clicking Go Home",  () => {
     const history = createMemoryHistory();
     const pushSpy = jest.spyOn(history, "push");
     render(
