185b08d35c40ba20df0736d9b81cef195b02fa62
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-04-26 16:44:53 -0500

    use canEdit instead

--- src/components/editMeasure/measureDetails/measureInformation/MeasureInformation.tsx
+++ src/components/editMeasure/measureDetails/measureInformation/MeasureInformation.tsx
@@ -34,6 +34,7 @@ export default function MeasureInformation() {
   const [toastOpen, setToastOpen] = useState<boolean>(false);
   const [toastMessage, setToastMessage] = useState<string>("");
   const [toastType, setToastType] = useState<string>("danger");
+  const canEdit = measure.createdBy === userName ? true : false;
 
   const onToastClose = () => {
     setToastType(null);
@@ -105,18 +106,18 @@ export default function MeasureInformation() {
       )}
       <DisplayDiv>
         <span tw="mr-2">Measure Name:</span>
-        {measure.createdBy === userName && (
+        {canEdit && (
           <InlineEdit
             text={measure.measureName}
             onSetText={updateMeasureTitle}
           />
         )}
-        {measure.createdBy !== userName && measure.measureName}
-        {measure.createdBy === userName && (
+        {!canEdit && measure.measureName}
+        {canEdit && (
           <Button
             variant="danger-primary"
             data-testid="delete-measure-button"
-            disabled={measure.createdBy !== userName}
+            disabled={!canEdit}
             onClick={() => setDeleteOpen(true)}
           >
             Delete Measure
