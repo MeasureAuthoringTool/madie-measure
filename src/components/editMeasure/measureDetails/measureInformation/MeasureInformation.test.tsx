f22bffec2fc513100c2cc4742ae47ba5ce8c1e92
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-04-26 13:18:33 -0500

    git check issue

--- src/components/editMeasure/measureDetails/measureInformation/MeasureInformation.test.tsx
+++ src/components/editMeasure/measureDetails/measureInformation/MeasureInformation.test.tsx
@@ -204,7 +204,7 @@ describe("MeasureInformation component", () => {
 
   it("Should not allow user to edit measure name if user is not the owner of measure", async () => {
     useOktaTokensMock.mockImplementation(() => ({
-      getUserName: () => "testuser2@example.com",
+      getUserName: () => "test.user2@example.com",
     }));
     render(<MeasureInformation />);
 
