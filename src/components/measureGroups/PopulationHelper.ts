6d88c7a52d4f5f6f229063af0e3c2b8c98c9fa72
adongare <ashok.dongare@semanticbits.com>
2022-07-27 10:27:20 -0400

    MAT-4566 update group page UI to work with model changes for MAT-4427-add second IP

---
+++ src/components/measureGroups/PopulationHelper.ts
@@ -0,0 +1,97 @@
+import { Population, PopulationType } from "@madie/madie-models";
+
+const initialPopulation = {
+  id: "",
+  name: PopulationType.INITIAL_POPULATION,
+  definition: "",
+};
+const denominator = {
+  id: "",
+  name: PopulationType.DENOMINATOR,
+  definition: "",
+};
+const denominatorExclusion = {
+  id: "",
+  name: PopulationType.DENOMINATOR_EXCLUSION,
+  definition: "",
+  optional: ["*"],
+};
+const denominatorException = {
+  id: "",
+  name: PopulationType.DENOMINATOR_EXCEPTION,
+  definition: "",
+  optional: ["Proportion"],
+};
+const numerator = {
+  id: "",
+  name: PopulationType.NUMERATOR,
+  definition: "",
+};
+const numeratorExclusion = {
+  id: "",
+  name: PopulationType.NUMERATOR_EXCLUSION,
+  definition: "",
+  optional: ["Proportion", "Ratio"],
+};
+const measurePopulation = {
+  id: "",
+  name: PopulationType.MEASURE_POPULATION,
+  definition: "",
+};
+const measurePopulationExclusion = {
+  id: "",
+  name: PopulationType.MEASURE_POPULATION_EXCLUSION,
+  definition: "",
+  optional: ["Continuous Variable"],
+};
+
+export const allPopulations = [
+  initialPopulation,
+  denominator,
+  denominatorExclusion,
+  denominatorException,
+  numerator,
+  numeratorExclusion,
+  measurePopulation,
+  measurePopulationExclusion,
+];
+
+// default populations for each scoring
+export const getPopulationsForScoring = (scoring: string): Population[] => {
+  let populations: Population[];
+  switch (scoring) {
+    case "Cohort":
+      populations = [initialPopulation];
+      break;
+    case "Proportion":
+      populations = [
+        initialPopulation,
+        denominator,
+        denominatorExclusion,
+        denominatorException,
+        numerator,
+        numeratorExclusion,
+      ];
+      break;
+    case "Continuous Variable":
+      populations = [
+        initialPopulation,
+        measurePopulation,
+        measurePopulationExclusion,
+      ];
+      break;
+    case "Ratio":
+      populations = [
+        initialPopulation,
+        denominator,
+        denominatorExclusion,
+        numerator,
+        numeratorExclusion,
+      ];
+      break;
+    default:
+      populations = [];
+  }
+
+  return populations;
+};
