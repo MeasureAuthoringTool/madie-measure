4ae00ce4f0e78bf225d916b966f9a208e71acc9b
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-08-02 12:47:18 -0400

    added test cases

--- src/components/measureGroups/MeasureGroups.test.tsx
+++ src/components/measureGroups/MeasureGroups.test.tsx
@@ -686,6 +686,109 @@ describe("Measure Groups Page", () => {
     );
   });
 
+  test("displaying a measure update warning modal while updating measure scoring and updating measure scoring for a measure group", async () => {
+    const newGroup = {
+      id: "7p03-5r29-7O0I",
+      scoring: "Continuous Variable",
+      populations: [
+        {
+          id: "id-1",
+          name: PopulationType.INITIAL_POPULATION,
+          definition: "Initial Population",
+        },
+        {
+          id: "id-2",
+          name: PopulationType.MEASURE_POPULATION,
+          definition: "Measure Population",
+        },
+      ],
+      groupDescription: "testDescription",
+      stratifications: [],
+      measureGroupTypes: ["Patient Reported Outcome"],
+      rateAggregation: "",
+      improvementNotation: "",
+    };
+    measure.groups = [newGroup];
+
+    renderMeasureGroupComponent();
+    expect(
+      (
+        screen.getByRole("option", {
+          name: "Continuous Variable",
+        }) as HTMLOptionElement
+      ).selected
+    ).toBe(true);
+
+    userEvent.selectOptions(
+      screen.getByTestId("scoring-unit-select"),
+      "Cohort"
+    );
+
+    expect(
+      (screen.getByRole("option", { name: "Cohort" }) as HTMLOptionElement)
+        .selected
+    ).toBe(true);
+
+    const definitionToUpdate =
+      "VTE Prophylaxis by Medication Administered or Device Applied";
+    userEvent.selectOptions(
+      screen.getByTestId("select-measure-group-population"),
+      screen.getByText(definitionToUpdate)
+    );
+    expect(
+      (
+        screen.getByRole("option", {
+          name: definitionToUpdate,
+        }) as HTMLOptionElement
+      ).selected
+    ).toBe(true);
+
+    expect(screen.getByRole("button", { name: "Save" })).toBeEnabled();
+
+    mockedAxios.put.mockResolvedValue({ data: { newGroup } });
+
+    const expectedGroup = {
+      id: "7p03-5r29-7O0I",
+      populations: [
+        {
+          id: "",
+          name: PopulationType.INITIAL_POPULATION,
+          definition:
+            "VTE Prophylaxis by Medication Administered or Device Applied",
+        },
+      ],
+      scoring: "Cohort",
+      groupDescription: "testDescription",
+      measureGroupTypes: ["Patient Reported Outcome"],
+      rateAggregation: "",
+      improvementNotation: "",
+      stratifications: [],
+    };
+
+    expect(screen.getByTestId("group-form-submit-btn")).toBeEnabled();
+
+    userEvent.click(screen.getByTestId("group-form-submit-btn"));
+
+    await waitFor(() => {
+      expect(
+        screen.getByTestId("update-measure-group-scoring-modal-agree-btn")
+      ).toBeInTheDocument();
+      expect(
+        screen.getByTestId("update-measure-group-scoring-modal-cancel-btn")
+      );
+    });
+
+    userEvent.click(
+      screen.getByTestId("update-measure-group-scoring-modal-agree-btn")
+    );
+
+    expect(mockedAxios.put).toHaveBeenCalledWith(
+      "example-service-url/measures/test-measure/groups/",
+      expectedGroup,
+      expect.anything()
+    );
+  });
+
   test("Onclicking discard button,should be able to discard the changes", async () => {
     group.id = "7p03-5r29-7O0I";
     group.groupDescription = "testDescription";
