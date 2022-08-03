eaed1bef7459193f1bec2e80d4e04657ba241ac1
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-08-03 12:00:23 -0400

    fixed test cases

--- src/components/editMeasure/EditMeasure.test.tsx
+++ src/components/editMeasure/EditMeasure.test.tsx
@@ -30,6 +30,7 @@ const measure = {
 
 const serviceApiMock = {
   fetchMeasure: jest.fn().mockResolvedValue(measure),
+  getAllPopulationBasisOptions: jest.fn().mockResolvedValue([]),
 } as unknown as MeasureServiceApi;
 
 useMeasureServiceApiMock.mockImplementation(() => {
