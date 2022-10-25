5b7975608893472c8566c3fcf2085e02e4a9fb4b
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-10-25 15:52:23 -0500

    remove unused imports

--- src/components/measureGroups/MultipleSelectDropdown.test.tsx
+++ src/components/measureGroups/MultipleSelectDropdown.test.tsx
@@ -1,11 +1,5 @@
 import * as React from "react";
-import {
-  render,
-  screen,
-  within,
-  fireEvent,
-  waitFor,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import MultipleSelectDropDown from "./MultipleSelectDropDown";
 import userEvent from "@testing-library/user-event";
 
