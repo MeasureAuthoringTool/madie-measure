6def59a39ce6fcb5604d8540f301f37526748070
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-10-25 09:37:12 -0400

    Added back data test id for test cases

--- src/components/measureGroups/MeasureGroups.tsx
+++ src/components/measureGroups/MeasureGroups.tsx
@@ -538,7 +538,9 @@ const MeasureGroups = () => {
           />
           <div tw="lg:col-span-5 pl-2 pr-2">
             <div tw="flex pb-2 pt-6">
-              <h2 tw="w-1/2">Population Criteria {measureGroupNumber + 1}</h2>
+              <h2 tw="w-1/2" data-testid="title">
+                Population Criteria {measureGroupNumber + 1}
+              </h2>
               <div tw="w-1/2 self-end">
                 <Typography
                   style={{
