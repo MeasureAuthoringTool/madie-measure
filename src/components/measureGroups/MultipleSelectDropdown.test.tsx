0a4a3246641f3fb53e79be88c6c3cb26ae319f18
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-10-25 15:49:42 -0500

    MAT-4614 add test

---
+++ src/components/measureGroups/MultipleSelectDropdown.test.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import {
+  render,
+  screen,
+  within,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react";
+import MultipleSelectDropDown from "./MultipleSelectDropDown";
+import userEvent from "@testing-library/user-event";
+
+describe("MultipleSelectDropDown Component", () => {
+  const selectOptions: string[] = ["Option 1", "Option 2"];
+  test("Should provide auto complete selection", () => {
+    render(
+      <MultipleSelectDropDown
+        formControl={null}
+        id="measure-group-type"
+        label="Type"
+        required={false}
+        disabled={false}
+        error={true}
+        options={selectOptions}
+        multipleSelect={false}
+        limitTags={2}
+      />
+    );
+
+    const measureGroupTypeSelect = screen.getByTestId(
+      "measure-group-type-dropdown"
+    );
+    expect(measureGroupTypeSelect).toBeInTheDocument();
+    const measureGroupTypeSelectButton = screen.getByRole("button", {
+      name: "Open",
+    });
+
+    userEvent.click(measureGroupTypeSelectButton);
+
+    expect(screen.getByText("Option 1")).toBeInTheDocument();
+    expect(screen.getByText("Option 2")).toBeInTheDocument();
+
+    userEvent.type(measureGroupTypeSelectButton, "Option 1");
+    expect(screen.getByText("Option 1")).toBeInTheDocument();
+    expect(screen.queryByText("Option 2")).not.toBeInTheDocument();
+  });
+});
