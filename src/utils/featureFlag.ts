5804e82f3827a1f473369ea22fea623fe4b0e4eb
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-12-16 11:05:34 -0500

    Added version button to action items of measure list

--- src/utils/featureFlag.ts
+++ src/utils/featureFlag.ts
@@ -1,9 +1,9 @@
-import React from "react";
-
 export function getFeatureFlag(feature: string) {
   //for now it is set to default false until feature flag is implemented
   if (feature === "export") {
     return false;
+  } else if (feature === "version") {
+    return true;
   }
   return false;
 }
