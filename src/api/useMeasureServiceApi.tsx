4d4670b4362eaad23b4aa02595d08964c3665bca
Ethan Kaplan <phaladone@gmail.com>
2022-12-19 12:44:56 -0800

    add encoding to URI

--- src/api/useMeasureServiceApi.tsx
+++ src/api/useMeasureServiceApi.tsx
@@ -249,7 +249,7 @@ export class MeasureServiceApi {
   ): Promise<any> {
     try {
       const response = await axios.get<any>(
-        `${this.baseUrl}/measures/search/${searchCriteria}`,
+        `${this.baseUrl}/measures/search/${encodeURI(searchCriteria)}`,
         {
           headers: {
             Authorization: `Bearer ${this.getAccessToken()}`,
