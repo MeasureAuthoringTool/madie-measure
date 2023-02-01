51afe23341e05bbe3b772e9bfc4885e5bcf404b6
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2023-02-01 12:23:01 -0500

    Updated test cases for populationCriteriaSideNav.tsx

--- src/components/measureGroups/populationCriteriaSideNav/PopulationCriteriaSideNav.test.tsx
+++ src/components/measureGroups/populationCriteriaSideNav/PopulationCriteriaSideNav.test.tsx
@@ -14,6 +14,11 @@ const measureGroups = [
     href: groupsBaseUrl,
     dataTestId: "leftPanelMeasureInformation-MeasureGroup1",
   },
+  {
+    title: "Criteria 2",
+    href: groupsBaseUrl,
+    dataTestId: "leftPanelMeasureInformation-MeasureGroup2",
+  },
 ];
 const initialProps: PopulationCriteriaSideNavProp = {
   canEdit: true,
@@ -39,7 +44,7 @@ const initialProps: PopulationCriteriaSideNavProp = {
   ],
   setSideNavLinks: jest.fn().mockImplementation((v) => v),
   measureId: "testMeasureId",
-  measureGroupNumber: 3,
+  measureGroupNumber: 0,
   setMeasureGroupNumber: jest.fn().mockImplementation((v) => v),
   isFormDirty: false,
 };
@@ -60,7 +65,7 @@ describe("PopulationCriteriaSideNav", () => {
     },
   };
 
-  const RenderEditMeasureSideBarNav = (props) => {
+  const RenderPopulationCriteriaSideNav = (props) => {
     return render(
       <MemoryRouter
         initialEntries={[{ pathname: "/measures/testMeasureId/edit/groups" }]}
@@ -72,108 +77,192 @@ describe("PopulationCriteriaSideNav", () => {
     );
   };
 
-  test("Measure's Population Criteria are loaded along with Supplemental Data and Risk Adjustment", async () => {
-    await waitFor(() => RenderEditMeasureSideBarNav(initialProps));
-    expect(getByText("Criteria 1")).toBeInTheDocument();
-    expect(getByText("Add Population Criteria")).toBeInTheDocument();
-    expect(getByText("Supplemental Data")).toBeInTheDocument();
-    expect(getByText("Risk Adjustment")).toBeInTheDocument();
+  it("Should load Population Criteria page along with Supplemental Data and Risk Adjustment", () => {
+    RenderPopulationCriteriaSideNav(initialProps);
+    expect(
+      screen.getByRole("button", {
+        name: /Population Criteria/i,
+      })
+    ).toBeInTheDocument();
     const criteria1 = screen.getByRole("link", {
       name: /Criteria 1/i,
     });
     expect(criteria1).toBeInTheDocument();
-    expect(criteria1.classList.contains("active")).toBeTruthy();
-    // expect(populationCriteria1.classList.contains("nav-link active")).toBe(
-    //   true
-    // );
+    // Initially state -> Criteria 1 will be active
+    expect(criteria1).toHaveClass("active");
+
+    expect(
+      screen.getByRole("button", {
+        name: /supplemental data/i,
+      })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", {
+        name: /risk adjustment/i,
+      })
+    ).toBeInTheDocument();
   });
 
-  test("Measure Group add click when dirty opens up a warning dialog, hitting cancel closes it", async () => {
-    RenderEditMeasureSideBarNav(initialProps);
-    expect(getByText("Criteria 1")).toBeInTheDocument();
+  it("Should collapse Population criteria tab", () => {
+    RenderPopulationCriteriaSideNav(initialProps);
+    const populationCriteriaCollapsableButton = screen.getByRole("button", {
+      name: /Population Criteria/i,
+    });
+    const criteria1 = screen.queryByRole("link", {
+      name: /Criteria 1/i,
+    });
+    expect(criteria1).toBeInTheDocument();
+    userEvent.click(populationCriteriaCollapsableButton);
+    expect(criteria1).not.toBeInTheDocument();
+  });
+
+  test("Measure Group add click when dirty opens up a warning dialog, hitting cancel closes it", () => {
+    RenderPopulationCriteriaSideNav({ ...initialProps, isFormDirty: true });
+    const criteria1 = screen.queryByRole("link", {
+      name: /Criteria 1/i,
+    });
+    expect(criteria1).toBeInTheDocument();
+    expect(criteria1).toHaveClass("active");
 
     expect(getByTestId("AddIcon")).toBeInTheDocument();
-    const addNewPopulationCriteriaButton = screen.getByTestId(
-      "add-measure-group-button"
-    ) as HTMLAnchorElement;
-    userEvent.click(addNewPopulationCriteriaButton);
+    const addNewPopulationCriteriaLink = screen.getByRole("link", {
+      name: "Add Population Criteria",
+    });
+    userEvent.click(addNewPopulationCriteriaLink);
 
-    const discardDialog = await getByTestId("discard-dialog");
+    const discardDialog = getByTestId("discard-dialog");
     expect(discardDialog).toBeInTheDocument();
-    const cancelButton = await screen.getByTestId(
-      "discard-dialog-cancel-button"
+    expect(discardDialog).toHaveTextContent("You have unsaved changes.");
+    expect(discardDialog).toHaveTextContent(
+      "Are you sure you want to discard your changes?"
     );
-    expect(queryByText("You have unsaved changes.")).toBeVisible();
+    const cancelButton = screen.getByRole("button", {
+      name: "No, Keep Working",
+    });
     expect(cancelButton).toBeInTheDocument();
     fireEvent.click(cancelButton);
+
+    // verifies user is not navigated
+    expect(criteria1).toHaveClass("active");
+  });
+
+  test("Measure Group add click when dirty opens up a warning dialog, hitting continue closes it and navigates", async () => {
+    const { rerender } = RenderPopulationCriteriaSideNav({
+      ...initialProps,
+      isFormDirty: true,
+    });
+    const criteria1 = screen.queryByRole("link", {
+      name: /Criteria 1/i,
+    });
+    expect(criteria1).toBeInTheDocument();
+    expect(criteria1).toHaveClass("active");
+
+    expect(getByTestId("AddIcon")).toBeInTheDocument();
+    const addNewPopulationCriteriaLink = screen.getByRole("link", {
+      name: "Add Population Criteria",
+    });
+    userEvent.click(addNewPopulationCriteriaLink);
+
+    const discardDialog = getByTestId("discard-dialog");
+    expect(discardDialog).toBeInTheDocument();
+    const continueButton = screen.getByRole("button", {
+      name: "Yes, Discard All Changes",
+    });
+    userEvent.click(continueButton);
+
+    /*
+      Onclick of continueButton, state of measureGroupNumber is updated, which is maintained by parent component.
+      work around -> rerendering the PopulationCriteriaSideNav component with a new measureGroupNumber
+    */
+    const newMeasureGroupLink = {
+      title: `Criteria 2`,
+      href: groupsBaseUrl,
+      dataTestId: `leftPanelMeasureInformation-MeasureGroup2`,
+    };
+    measureGroups.push(newMeasureGroupLink);
+    // initialProps.sideNavLinks[0].groups = measureGroups;
+    const reRenderProps = { ...initialProps, measureGroupNumber: 2 };
+    rerender(
+      <MemoryRouter
+        initialEntries={[{ pathname: "/measures/testMeasureId/edit/groups" }]}
+      >
+        <ServiceContext.Provider value={serviceConfig}>
+          <PopulationCriteriaSideNav {...reRenderProps} />
+        </ServiceContext.Provider>
+      </MemoryRouter>
+    );
+
     await waitFor(() => {
-      expect(queryByText("You have unsaved changes.")).not.toBeVisible();
+      const criteria3 = screen.getByRole("link", {
+        name: /Criteria 3/i,
+      });
+      expect(criteria3).toHaveClass("active");
     });
   });
 
-  test("Measure Group nav click when dirty opens up a warning dialog, hitting cancel closes it", async () => {
-    await waitFor(() => RenderEditMeasureSideBarNav(initialProps));
-    expect(getByText("Criteria 1")).toBeInTheDocument();
-    const navButton = getByTestId("leftPanelMeasureInformation-MeasureGroup1");
-    expect(navButton).toBeInTheDocument();
-    fireEvent.click(navButton);
-    const discardDialog = await getByTestId("discard-dialog");
+  test("Measure Group nav click when dirty opens up a warning dialog, hitting cancel closes it", () => {
+    RenderPopulationCriteriaSideNav({ ...initialProps, isFormDirty: true });
+    const criteria1 = screen.queryByRole("link", {
+      name: /Criteria 1/i,
+    });
+    expect(criteria1).toBeInTheDocument();
+    expect(criteria1).toHaveClass("active");
+
+    const criteria2 = screen.queryByRole("link", {
+      name: /Criteria 2/i,
+    });
+    userEvent.click(criteria2);
+
+    const discardDialog = getByTestId("discard-dialog");
     expect(discardDialog).toBeInTheDocument();
-    const cancelButton = await screen.getByTestId(
-      "discard-dialog-cancel-button"
+    expect(discardDialog).toHaveTextContent("You have unsaved changes.");
+    expect(discardDialog).toHaveTextContent(
+      "Are you sure you want to discard your changes?"
     );
-    expect(queryByText("You have unsaved changes.")).toBeVisible();
+    const cancelButton = screen.getByRole("button", {
+      name: "No, Keep Working",
+    });
     expect(cancelButton).toBeInTheDocument();
     fireEvent.click(cancelButton);
-    await waitFor(() => {
-      expect(queryByText("You have unsaved changes.")).not.toBeVisible();
-    });
+    // verifies user is not navigated
+    expect(criteria1).toHaveClass("active");
   });
 
-  test("Measure Group nav click when dirty opens up a warning dialog, hitting continue closes it", async () => {
-    await waitFor(() => RenderEditMeasureSideBarNav(initialProps));
-    expect(getByText("Criteria 1")).toBeInTheDocument();
-    const navButton = getByTestId("leftPanelMeasureInformation-MeasureGroup1");
-    expect(navButton).toBeInTheDocument();
-    fireEvent.click(navButton);
-    const discardDialog = await getByTestId("discard-dialog");
+  test("Measure Group nav click when dirty opens up a warning dialog, hitting continue closes it and navigates", () => {
+    const { rerender } = RenderPopulationCriteriaSideNav({
+      ...initialProps,
+      isFormDirty: true,
+    });
+    const criteria1 = screen.queryByRole("link", {
+      name: /Criteria 1/i,
+    });
+    expect(criteria1).toBeInTheDocument();
+    expect(criteria1).toHaveClass("active");
+
+    const criteria2 = screen.queryByRole("link", {
+      name: /Criteria 2/i,
+    });
+    userEvent.click(criteria2);
+
+    const discardDialog = getByTestId("discard-dialog");
     expect(discardDialog).toBeInTheDocument();
-    const continueButton = await screen.getByTestId(
-      "discard-dialog-continue-button"
-    );
-    expect(queryByText("You have unsaved changes.")).toBeVisible();
-    expect(continueButton).toBeInTheDocument();
+    const continueButton = screen.getByRole("button", {
+      name: "Yes, Discard All Changes",
+    });
     fireEvent.click(continueButton);
-    await waitFor(() => {
-      expect(queryByText("You have unsaved changes.")).not.toBeVisible();
-    });
-  });
 
-  test("Measure navigation click without dirty does not render a dialog", async () => {
-    const nonDirtyProps = { ...initialProps, dirty: false };
-    RenderEditMeasureSideBarNav(nonDirtyProps);
-    expect(getByText("Criteria 1")).toBeInTheDocument();
-    const navButton = getByTestId("leftPanelMeasureInformation-MeasureGroup1");
-    expect(navButton).toBeInTheDocument();
-    fireEvent.click(navButton);
-    expect(queryByText("You have unsaved changes.")).toBe(null);
-  });
-
-  test("Population Criteria page navigation between the tabs", async () => {
-    RenderEditMeasureSideBarNav(initialProps);
-    expect(
-      getByTestId("leftPanelMeasurePopulationCriteriaTab")
-    ).toBeInTheDocument();
-    expect(
-      getByTestId("leftPanelMeasurePopulationsSupplementalDataTab")
-    ).toBeInTheDocument();
-    expect(
-      getByTestId("leftPanelMeasurePopulationsRiskAdjustmentTab")
-    ).toBeInTheDocument();
-    expect(getByText("Criteria 1")).toBeInTheDocument();
-    userEvent.click(
-      getByTestId("leftPanelMeasurePopulationsSupplementalDataTab")
+    // Since we cannot update state of measureGroupNumber which is maintained by parent component.
+    // rerendering the PopulationCriteriaSideNav component with a new measureGroupNumber
+    const reRenderProps = { ...initialProps, measureGroupNumber: 1 };
+    rerender(
+      <MemoryRouter
+        initialEntries={[{ pathname: "/measures/testMeasureId/edit/groups" }]}
+      >
+        <ServiceContext.Provider value={serviceConfig}>
+          <PopulationCriteriaSideNav {...reRenderProps} />
+        </ServiceContext.Provider>
+      </MemoryRouter>
     );
-    expect(getByText("Supplemental Data")).toBeInTheDocument();
+    expect(criteria2).toHaveClass("active");
   });
 });
