ab1d48802ff09aec9903c6a42f55b6dfbea00df4
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2023-03-08 09:04:46 -0500

    removed unwanted code

--- src/utils/measureModelHelpers.test.tsx
+++ src/utils/measureModelHelpers.test.tsx
@@ -1,43 +1,13 @@
-import { Measure } from "@madie/madie-models";
 import getModelFamily from "./measureModelHelpers";
-
-const measures: Measure[] = [
-  {
-    id: "IDIDID1",
-    measureHumanReadableId: "humanId",
-    ecqmTitle: "ecqmTitleeee",
-    measureSetId: "1",
-    version: "0.0.000",
-    createdAt: "",
-    createdBy: "",
-    model: "QDM v4.1.1",
-    active: true,
-    measureMetaData: {
-      draft: true,
-    },
-  },
-  {
-    id: "IDIDID2",
-    measureSetId: "2",
-    version: "0.0.000",
-    state: "DRAFT",
-    measureName: "draft measure - B",
-    model: "QI-Core v5.0.0 ",
-    active: false,
-    measureMetaData: {
-      draft: true,
-    },
-  },
-];
 
 describe("Check Modal Family", () => {
   it("checking if the model family is same as what provided when the model is not QI-Core", () => {
-    const modelFamily = getModelFamily(measures[0].model);
+    const modelFamily = getModelFamily("QDM v4.0.000");
     expect(modelFamily).toBe("QDM4");
   });
 
   it("checking if the model family is maped to FHIR when the measure model is QI-Core", () => {
-    const modelFamily = getModelFamily(measures[1].model);
+    const modelFamily = getModelFamily("QI-Core v5.0.000");
     expect(modelFamily).toBe("FHIR5");
   });
 });
