52d19ea8e3dc32962200c77852e4f1fe9e39f0e9
mcmcphillips <matt.mcphillips@semanticbits.com>
2023-03-16 07:42:49 -0700

    Update review-info for 508 compliance and empty display ac

--- src/components/editMeasure/reviewInfo/ReviewInfoItem.tsx
+++ src/components/editMeasure/reviewInfo/ReviewInfoItem.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import info from "../../../components/common/info.svg";
 const ReviewInfoItem = ({ label, helperText, date, testId }) => {
-  const dateTime = new Date(date).toLocaleDateString();
+  // if date omitted we display a - instead.
+  const dateTime = date ? new Date(date).toLocaleDateString() : "-";
   return (
     <article className="info-item">
       <div>
         <div className="help-image">
-          <label>{label}</label>
+          <label id={`${testId}-label`} htmlFor={testId}>
+            {label}
+          </label>
           <img alt="info help" src={info} />
-          <span className="more-text">{helperText}</span>
+          <span className="more-text" id={`${testId}-helper-text`}>
+            {helperText}
+          </span>
         </div>
       </div>
-      <input data-testId={testId} value={dateTime} readOnly />
+      <input
+        data-testId={testId}
+        value={dateTime}
+        readOnly
+        aria-describedby={`${testId}-helper-text`}
+      />
     </article>
   );
 };
