ad8246b593c9205831dba97bc7182711cb4708fc
mcmcphillips <matt.mcphillips@icf.com>
2024-02-07 10:36:15 -0800

    MAT-6692: Remove character limit on references.refText, def.term, def.definition

--- src/components/editMeasure/details/MeasureReferences/MeasureReferencesValidator.ts
+++ src/components/editMeasure/details/MeasureReferences/MeasureReferencesValidator.ts
@@ -2,7 +2,5 @@ import * as Yup from "yup";
 
 export const MeasureReferencesValidator = Yup.object().shape({
   referenceType: Yup.string().required("Measure Reference Type is required."),
-  referenceText: Yup.string()
-    .required("Measure Reference is required.")
-    .max(250, "Measure Reference cannot be more than 250 characters."),
+  referenceText: Yup.string().required("Measure Reference is required."),
 });
