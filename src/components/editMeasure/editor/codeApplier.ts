08d912ff13870d06be0f291d2fcbe2ae427c67b8
Greg Akins <gregory.akins@icf.com>
2024-04-25 09:00:26 -0400

    MAT-6853: Fixing some formatting with adding lines to cql

--- src/components/editMeasure/editor/codeApplier.ts
+++ src/components/editMeasure/editor/codeApplier.ts
@@ -1,9 +1,4 @@
-import {
-  CqlAntlr,
-  CqlResult,
-  CqlCodeSystem,
-} from "@madie/cql-antlr-parser/dist/src";
-import { parse } from "date-fns";
+import { CqlAntlr, CqlResult } from "@madie/cql-antlr-parser/dist/src";
 
 const applyCode = (cql: string, code: any): string => {
   const cqlArr: string[] = cql.split("\n");
@@ -12,7 +7,6 @@ const applyCode = (cql: string, code: any): string => {
 
   // Let's check if the code set is already in the CQL
   let codeSystemExists: boolean = false;
-
   if (parseResults.codeSystems.length > 0) {
     parseResults.codeSystems.forEach((codesystem) => {
       const oldCodeSystemName = codesystem.name.replace(/["']/g, "");
@@ -25,13 +19,15 @@ const applyCode = (cql: string, code: any): string => {
   }
 
   if (!codeSystemExists) {
+    //Add codeSystem to CQL
     const codeSystemAddition = `codesystem "${code.codeSystem}": 'urn:oid:${code.codeSystemOid}'`;
-    const cqlLine =
-      parseResults.codeSystems[parseResults.codeSystems.length - 1].stop.line;
-    cqlArr.splice(cqlLine, 0, codeSystemAddition);
+    cqlArr.splice(
+      findCodeSystemInsertPoint(parseResults),
+      0,
+      codeSystemAddition
+    );
   }
 
-  //add code to CQL
   //check to see if the code already exists.. add it if it doesn't
   let codeExists: boolean = false;
   if (parseResults.codes.length > 0) {
@@ -47,19 +43,22 @@ const applyCode = (cql: string, code: any): string => {
           oldCodeCodeSystem === code.codeSystem);
     });
   }
+
   if (!codeExists) {
+    //add code to CQL
     cqlArr.splice(
-      findInsertPoint(parseResults),
+      findCodeInsertPoint(parseResults),
       0,
       `code "${code.display}": '${code.name}' from "${code.codeSystem}" display '${code.display}'`
     );
+    //display toast message?
   }
 
   //return the array as a string
-  return cqlArr.join("\r\n");
+  return cqlArr.join("\n");
 };
 
-const findInsertPoint = (parseResults: CqlResult) => {
+const findCodeInsertPoint = (parseResults: CqlResult) => {
   const codes: number = parseResults.codes.length;
   const codesystems: number = parseResults.codeSystems.length;
   const includes: number = parseResults.includes.length;
@@ -68,6 +67,24 @@ const findInsertPoint = (parseResults: CqlResult) => {
   if (codes > 0) {
     return parseResults.codes[parseResults.codes.length - 1].stop.line;
   } else if (codesystems > 0) {
+    return parseResults.codeSystems[parseResults.codeSystems.length - 1].stop
+      .line;
+  } else if (includes > 0) {
+    return parseResults.includes[parseResults.includes.length - 1].stop.line;
+  } else if (usings > 0) {
+    return parseResults.using.start.line;
+  } else {
+    return 1;
+  }
+};
+
+const findCodeSystemInsertPoint = (parseResults: CqlResult) => {
+  const codes: number = parseResults.codes.length;
+  const codesystems: number = parseResults.codeSystems.length;
+  const includes: number = parseResults.includes.length;
+  const usings: number = parseResults.using.start.line;
+
+  if (codesystems > 0) {
     return parseResults.codeSystems[parseResults.codeSystems.length - 1].stop
       .line;
   } else if (includes > 0) {
