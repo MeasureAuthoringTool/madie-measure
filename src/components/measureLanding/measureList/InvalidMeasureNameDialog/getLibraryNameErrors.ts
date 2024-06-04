1aad1fdfa5bdc10d547bfade81e2414396e61d3a
mcmcphillips <matt.mcphillips@icf.com>
2024-06-04 08:08:32 -0700

    MAT-7206: Implement workflow for bad cql library name

---
+++ src/components/measureLanding/measureList/InvalidMeasureNameDialog/getLibraryNameErrors.ts
@@ -0,0 +1,51 @@
+import * as Yup from "yup";
+import { Model } from "@madie/madie-models";
+
+export const ERROR_MAP = {
+  required: {
+    test: (name: string, model: Model) => {
+      return name.length > 0;
+    },
+    message: "Library name is required.",
+  },
+  max: {
+    test: (name: string, model: Model) => {
+      return name.length <= 64;
+    },
+    message: "Library name cannot be more than 64 characters.",
+  },
+  qdmCheck: {
+    test: (name: string, model: Model) => {
+      if (model !== Model.QDM_5_6) {
+        return true;
+      }
+      return /^[A-Z][a-zA-Z0-9_]*$/.test(name);
+    },
+    message:
+      "Library name must start with an upper case letter, followed by alpha-numeric character(s) and must not contain spaces or other special characters except of underscore for QDM.",
+  },
+  qiCoreCheck: {
+    test: (name: string, model: Model) => {
+      if (model !== Model.QICORE) {
+        return true;
+      }
+      return /^[A-Z][a-zA-Z0-9]*$/.test(name);
+    },
+    message:
+      "Library name must start with an upper case letter, followed by alpha-numeric character(s) and must not contain spaces or other special characters.",
+  },
+};
+
+const getLibraryNameErrors = (libraryName: string, model: Model) => {
+  const errorList = [];
+
+  for (const errorInfo in ERROR_MAP) {
+    const testMatch = ERROR_MAP[errorInfo].test(libraryName, model);
+    if (!testMatch) {
+      errorList.push(ERROR_MAP[errorInfo].message);
+    }
+  }
+  return errorList;
+};
+
+export default getLibraryNameErrors;
