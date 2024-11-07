e2262d352c2e75abb587ed954c2d53288c5cff17
sb-cecilialiu <cecilia.liu@icf.com>
2024-11-07 07:05:05 -0600

    MAT-7698_saveEditedParameter

--- src/types/madie-madie-editor.d.ts
+++ src/types/madie-madie-editor.d.ts
@@ -76,6 +76,10 @@ declare module "@madie/madie-editor" {
     onChange?: (value: string) => void;
     handleApplyCode?: (code: Code) => void;
     handleApplyParameter?: (parameter: Parameter) => void;
+    handleParameterEdit?: (
+      parameter: Parameter,
+      parameterToApply: Parameter
+    ) => void;
     handleApplyValueSet?: (vs: ValueSetForSearch) => void;
     handleApplyDefinition?: (def: Definition) => void;
     handleDefinitionEdit?: (lib: SelectedLibrary, def: Definition) => void;
