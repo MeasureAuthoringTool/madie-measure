c8a0e92220d8126b3d1baac28982a69fe07f7081
sb-cecilialiu <cecilia.liu@icf.com>
2024-11-11 11:09:13 -0600

    MAT-7698 add edit parameter test

--- src/__mocks__/@madie/madie-editor.tsx
+++ src/__mocks__/@madie/madie-editor.tsx
@@ -32,6 +32,7 @@ export function MadieTerminologyEditor({
   handleApplyLibrary,
   handleEditLibrary,
   handleDeleteLibrary,
+  handleParameterEdit,
 }) {
   const code = {
     codeSystem: "RXNORM",
@@ -54,6 +55,15 @@ export function MadieTerminologyEditor({
     name: "TestHelpers",
     version: "1.0.000",
     alias: "EditedHelpers",
+  };
+
+  const parameter = {
+    parameterName: "Measurement Period",
+    expression: "Interval<System.DateTime",
+  };
+  const parameterToApply = {
+    parameterName: "Test Measurement Period",
+    expression: "Interval<System.DateTime>",
   };
 
   return (
@@ -93,6 +103,13 @@ export function MadieTerminologyEditor({
         onClick={() => handleDeleteLibrary(library)}
       >
         Delete Library
+      </button>
+
+      <button
+        data-testid="edit-parameter"
+        onClick={() => handleParameterEdit(parameter, parameterToApply)}
+      >
+        Edit Parameter
       </button>
     </>
   );
