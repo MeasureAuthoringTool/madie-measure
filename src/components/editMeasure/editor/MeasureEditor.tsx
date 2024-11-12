be4c4907b300d2d466378e413f0aafe0f8a950c4
sb-cecilialiu <cecilia.liu@icf.com>
2024-11-12 17:16:28 -0500

    MAT-7699 delete parameter change success message

--- src/components/editMeasure/editor/MeasureEditor.tsx
+++ src/components/editMeasure/editor/MeasureEditor.tsx
@@ -564,7 +564,7 @@ const MeasureEditor = () => {
     handleMadieEditorValue(updatedCql);
     const setParameterConfirmation = () => {
       setToastMessage(
-        `Parameter ${parameter.parameterName} has been successfully updated to the CQL`
+        `Parameter ${parameter.parameterName} has been successfully removed from the CQL.`
       );
       setToastType("success");
       setToastOpen(true);
