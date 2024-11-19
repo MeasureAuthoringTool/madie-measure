b59cde5de8dc006f6c74d611afc12a0c018c0005
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-11-19 14:21:59 -0800

    MAT-7093 standardize icon

--- src/components/editMeasure/details/MeasureMetaDataRow.tsx
+++ src/components/editMeasure/details/MeasureMetaDataRow.tsx
@@ -4,7 +4,7 @@ import "twin.macro";
 import "styled-components/macro";
 import { Popover } from "@madie/madie-design-system/dist/react";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
-import EditNoteIcon from "@mui/icons-material/EditNote";
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import { blue, red } from "@mui/material/colors";
 import { Tooltip } from "@mui/material";
 
@@ -18,8 +18,6 @@ interface MeasureMetaDataRowProps {
 
 const MeasureMetaDataRow = (props: MeasureMetaDataRowProps) => {
   const { name, description, id, handleClick, canEdit } = props;
-  const [optionsOpen, setOptionsOpen] = useState<boolean>(false);
-  const [anchorEl, setAnchorEl] = useState(null);
 
   return (
     <>
@@ -30,13 +28,12 @@ const MeasureMetaDataRow = (props: MeasureMetaDataRowProps) => {
           <td style={{ width: 160 }}>
             <Tooltip
               data-testid="delete-tooltip"
-              title={"Delete"}
-              placement={"top"}
+              title="Delete"
+              placement="top"
               arrow
             >
               <DeleteOutlinedIcon
-                onClick={(e) => {
-                  e.preventDefault();
+                onClick={() => {
                   handleClick(id, "delete");
                 }}
                 data-testid={`delete-measure-reference-${id}`}
@@ -47,13 +44,12 @@ const MeasureMetaDataRow = (props: MeasureMetaDataRowProps) => {
 
             <Tooltip
               data-testid="edit-tooltip"
-              title={"Edit"}
-              placement={"top"}
+              title="Edit"
+              placement="top"
               arrow
             >
-              <EditNoteIcon
-                onClick={(e) => {
-                  e.preventDefault();
+              <BorderColorOutlinedIcon
+                onClick={() => {
                   handleClick(id, "edit");
                 }}
                 data-testid={`edit-measure-reference-${id}`}
