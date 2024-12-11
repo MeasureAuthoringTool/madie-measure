c1a9e5471260379ff1d1d99e6bcd9bcbb6368a9a
Edwin Tan <N119494@icf.com>
2024-12-11 14:36:02 -0800

    MAT-7898: add dropdown for test cases

--- src/components/editMeasure/testCases/components/editTestCase/EditTestCaseBreadCrumbs.test.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/EditTestCaseBreadCrumbs.test.tsx
@@ -4,10 +4,142 @@ import { describe, expect, test } from "@jest/globals";
 import { MemoryRouter } from "react-router-dom";
 import EditTestCaseBreadCrumbs from "./EditTestCaseBreadCrumbs";
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+// @ts-ignore
+import { measureStore } from "@madie/madie-util";
+import {
+  GroupPopulation,
+  Measure,
+  MeasureScoring,
+  PopulationExpectedValue,
+  TestCase,
+} from "@madie/madie-models";
 
 const { getByText } = screen;
 
+jest.mock("@madie/madie-util", () => ({
+  useDocumentTitle: jest.fn(),
+  useOktaTokens: jest.fn(() => ({
+    getAccessToken: () => "test.jwt",
+  })),
+  checkUserCanEdit: jest.fn(),
+  useFeatureFlags: () => ({}),
+  measureStore: {
+    updateMeasure: jest.fn((measure) => measure),
+    state: jest.fn().mockImplementation(() => null),
+    initialState: jest.fn().mockImplementation(() => null),
+    subscribe: () => {
+      return { unsubscribe: () => null };
+    },
+  },
+  routeHandlerStore: {
+    subscribe: () => {
+      return { unsubscribe: () => null };
+    },
+    updateRouteHandlerState: jest.fn((routeObj) => routeObj),
+    state: { canTravel: false, pendingPath: "" },
+    initialState: { canTravel: false, pendingPath: "" },
+  },
+}));
+
+const testCases = [
+  {
+    id: "1",
+    description: "Test IPP",
+    title: "WhenAllGood",
+    series: "IPP-Pass",
+    validResource: true,
+    json: "{}",
+    groupPopulations: [
+      {
+        groupId: "1",
+        scoring: MeasureScoring.PROPORTION,
+        populationValues: [
+          {
+            name: "initialPopulation",
+            expected: true,
+          },
+          {
+            name: "denominator",
+            expected: false,
+          },
+          {
+            name: "numerator",
+            expected: true,
+          },
+        ] as PopulationExpectedValue[],
+      },
+    ] as GroupPopulation[],
+  },
+  {
+    id: "2",
+    description: "Test IPP Fail when something is wrong",
+    title: "WhenSomethingIsWrong",
+    series: "IPP-Fail",
+    validResource: true,
+    json: "{}",
+    groupPopulations: [
+      {
+        groupId: "1",
+        scoring: MeasureScoring.PROPORTION,
+        populationValues: [
+          {
+            name: "initialPopulation",
+            expected: false,
+          },
+          {
+            name: "denominator",
+            expected: false,
+          },
+          {
+            name: "numerator",
+            expected: true,
+          },
+        ] as PopulationExpectedValue[],
+      },
+    ] as GroupPopulation[],
+  },
+  {
+    id: "3",
+    description: "Invalid test case",
+    title: "WhenJsonIsInvalid",
+    series: "IPP-Fail",
+    validResource: false,
+    json: "{}",
+    groupPopulations: [
+      {
+        groupId: "1",
+        scoring: MeasureScoring.PROPORTION,
+        populationValues: [
+          {
+            name: "initialPopulation",
+            expected: false,
+          },
+          {
+            name: "denominator",
+            expected: false,
+          },
+          {
+            name: "numerator",
+            expected: true,
+          },
+        ] as PopulationExpectedValue[],
+      },
+    ] as GroupPopulation[],
+  },
+] as TestCase[];
+
+const measure = {
+  id: "measure ID",
+  createdBy: "testuser@example.com",
+  model: "QI-Core v4.1.1",
+  testCases: testCases,
+} as Measure;
+
 describe("EditTestCaseBreadCrumbs", () => {
+  beforeEach(() => {
+    measureStore.state.mockImplementation(() => measure);
+  });
+
   test("routes to a new route, and navigates back.", async () => {
     render(
       <MemoryRouter>
@@ -23,14 +155,10 @@ describe("EditTestCaseBreadCrumbs", () => {
         />
       </MemoryRouter>
     );
+
     fireEvent.click(getByText("Test Cases"));
     await waitFor(() => {
       expect(screen.getByText("Test Cases")).toHaveClass("madie-link");
-    });
-
-    fireEvent.click(getByText("title"));
-    await waitFor(() => {
-      expect(screen.getByText("title")).toHaveClass("madie-link active");
     });
   });
 });
--- src/components/editMeasure/testCases/components/editTestCase/EditTestCaseBreadCrumbs.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/EditTestCaseBreadCrumbs.tsx
@@ -1,43 +1,128 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import { TestCase } from "@madie/madie-models";
-import { NavLink } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { Menu, MenuItem } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { measureStore } from "@madie/madie-util";
 import "./EditTestCaseBreadCrumbs.scss";
 
 export interface EditTestCaseBreadCrumbsProps {
   measureId: string;
   testCase: TestCase;
 }
+
 const EditTestCaseBreadCrumbs = (props: EditTestCaseBreadCrumbsProps) => {
   const { testCase, measureId } = props;
 
-  let testCaseString = "";
-  if (testCase) {
-    testCaseString = testCase?.caseNumber
-      ? `Case #${testCase.caseNumber}: `
-      : "";
-    testCaseString += testCase?.series
-      ? `${testCase.series} - ${testCase.title}`
-      : `${testCase.title}`;
-  }
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [testCases, setTestCases] = React.useState(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const [measure, setMeasure] = useState<any>(measureStore.state);
+
+  const generateTestCaseString = useCallback((testCase) => {
+    let testCaseString = "";
+
+    if (testCase) {
+      testCaseString = testCase?.caseNumber
+        ? `Case #${testCase.caseNumber}: `
+        : "";
+      testCaseString += testCase?.series
+        ? `${testCase.series} - ${testCase.title}`
+        : `${testCase.title}`;
+    }
+
+    return testCaseString;
+  }, []);
+
+  let testCaseString = generateTestCaseString(testCase);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const subscription = measureStore.subscribe(setMeasure);
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (measure && testCaseString) {
+      const measureTestCases = measure.testCases
+        ?.map((testCase) => {
+          return {
+            ...testCase,
+            testCaseString: generateTestCaseString(testCase),
+          };
+        })
+        .sort((a, b) => b.caseNumber - a.caseNumber);
+
+      setTestCases(measureTestCases);
+
+      const index = measureTestCases?.findIndex(
+        (testCase) => testCase.testCaseString === testCaseString
+      );
+      setSelectedIndex(index);
+    }
+  }, [measure, testCaseString]);
+
+  const handleIconButtonOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (index: number) => {
+    setAnchorEl(null);
+
+    const newPath = `/measures/${measure.id}/edit/test-cases/${testCases[index].id}`;
+    navigate(newPath);
+  };
+
   return (
     <div id="edit-test-case-bread-crumbs">
       <NavLink
         data-testid="qdm-test-cases"
         to={`/measures/${measureId}/edit/test-cases/list-page`}
-        className={({ isActive }) => (isActive ? "madie-link" : "madie-link")}
+        className="madie-link"
       >
         Test Cases
       </NavLink>
       <div className="spacer">/</div>
-      <NavLink
-        data-testid="qdm-test-cases-testcase"
-        to={`/measures/${measureId}/edit/test-cases/${testCase?.id}`}
-        className={({ isActive, isPending }) =>
-          isActive ? "madie-link active" : "madie-link"
-        }
+      {testCaseString}
+
+      <Button
+        aria-label="Navigate test cases button"
+        data-testid="navigate-test-cases-btn"
+        onClick={handleIconButtonOpen}
       >
-        {testCaseString}
-      </NavLink>
+        <ExpandMoreIcon style={{ height: 32, width: 32, color: "#333" }} />
+      </Button>
+
+      <Menu
+        id="test-cases-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        {testCases?.map((testCase, index) => {
+          const testCaseString = generateTestCaseString(testCase);
+
+          return (
+            <MenuItem
+              onClick={() => handleMenuItemClick(index)}
+              key={testCaseString}
+              value={testCaseString}
+              selected={index === selectedIndex}
+              disabled={index === selectedIndex}
+            >
+              {testCaseString}
+            </MenuItem>
+          );
+        })}
+      </Menu>
     </div>
   );
 };
