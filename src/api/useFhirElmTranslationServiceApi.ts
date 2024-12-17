80cf6588a946f5a79f697955645bcc47a6296c52
nmorasb <nate.moraca@semanticbits.com>
2024-12-17 09:18:28 -0500

    MAT-7801: filter displayed QI-Core elements based on measure CQL, always show Patient, do not restrict if no elements found

--- src/api/useFhirElmTranslationServiceApi.ts
+++ src/api/useFhirElmTranslationServiceApi.ts
@@ -2,6 +2,17 @@ import axios from "./axios-instance";
 import useServiceConfig from "./useServiceConfig";
 import { ServiceConfig } from "./ServiceContext";
 import { useOktaTokens } from "@madie/madie-util";
+import { Measure } from "@madie/madie-models";
+
+export interface SourceDataCriteria {
+  oid: string;
+  title: string;
+  description: string;
+  type: string;
+  drc: string;
+  codeId: string;
+  name: string;
+}
 
 export class FhirElmTranslationServiceApi {
   constructor(private baseUrl: string, private getAccessToken: () => string) {}
@@ -23,6 +34,29 @@ export class FhirElmTranslationServiceApi {
     } catch (err) {
       console.error("unable to retrieve translator version:", err);
       throw new Error(err);
+    }
+  }
+
+  async fetchRelevantDataElements(
+    measure: Measure
+  ): Promise<Array<SourceDataCriteria>> {
+    try {
+      const response = await axios.put(
+        `${this.baseUrl}/fhir/cql/relevant-elements`,
+        measure,
+        {
+          headers: {
+            Authorization: `Bearer ${this.getAccessToken()}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(
+        error.message ||
+          "An Error occurred while fetching relevant data elements"
+      );
     }
   }
 }
