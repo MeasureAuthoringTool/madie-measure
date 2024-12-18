e46f93c3bc18554c7e94d23594c1fd53854fbcea
sb-cecilialiu <cecilia.liu@icf.com>
2024-12-18 14:23:59 -0600

    MAT-7795 add test coverage

--- src/components/editMeasure/editor/cqlFunctionApplier.test.ts
+++ src/components/editMeasure/editor/cqlFunctionApplier.test.ts
@@ -203,4 +203,27 @@ describe("Definition Apply Function tests", () => {
     const result = deleteCQLFunction(mockCql, testFunction);
     expect(result.message).toBe("Function test has not been defined in CQL.");
   });
+
+  it("Will delete a function successfully", () => {
+    const mockCql = `library TestLib version '0.0.000'
+using QICore version '4.1.1'
+include FHIRHelpers version '4.1.000' called FHIRHelpers
+
+context Patient
+
+define function MeasureObservation(e Encounter):
+  2`;
+
+    const functionToDelete = {
+      functionName: "MeasureObservation",
+      fluentFunction: false,
+      expressionValue: "define function MeasureObservation(e Encounter):\n  2",
+      expression: "define function MeasureObservation(e Encounter):\n  2",
+    };
+
+    const result = deleteCQLFunction(mockCql, functionToDelete);
+    expect(result.message).toBe(
+      "Function MeasureObservation has been successfully removed from the CQL."
+    );
+  });
 });
