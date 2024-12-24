8ca8f81513ff0379a3cb8423b17a968c4016d921
Prateek Keerthi <prateekkeerthi7@gmail.com>
2024-12-24 11:07:56 -0500

    MAT-7674 added URL field to testcase builder

---
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/UrlComponent.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { TypeComponentProps } from "./TypeComponentProps";
+import { TextField } from "@madie/madie-design-system/dist/react/";
+
+const UrlComponent = ({
+  canEdit,
+  fieldRequired,
+  value,
+  onChange,
+  label = "URL",
+  structureDefinition,
+}: TypeComponentProps) => {
+  return (
+    <TextField
+      required={fieldRequired}
+      disabled={!canEdit}
+      id={`url-field-${label}`}
+      label={label}
+      placeholder={label}
+      inputProps={{
+        "data-testid": `url-input-field-${label}`,
+        "aria-describedby": `url-input-field-helper-text-${label}`,
+        required: fieldRequired,
+        "aria-required": fieldRequired,
+      }}
+      data-testid={`url-field-${label}`}
+      size="small"
+      fullWidth
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
+
+export default UrlComponent;
