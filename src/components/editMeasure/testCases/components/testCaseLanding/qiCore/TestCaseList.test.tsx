c9e4aa71521aae1e85f14f66b79e998c215141f6
Ethan Kaplan <47760891+ethankaplan@users.noreply.github.com>
2025-01-17 06:59:30 -0800

    Update src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseList.test.tsx
    Co-authored-by: Ashok Dongare <ashok.dongare@semanticbits.com>

--- src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseList.test.tsx
+++ src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseList.test.tsx
@@ -1882,7 +1882,7 @@ describe("TestCaseList component", () => {
     });
   });
   describe("TestCaseList component with deleteMultipleTestCases", () => {
-    it("should render list of test cases with checkboxes if flag is true", async () => {
+    it("should delete selected test cases if the flag is true", async () => {
       useTestCaseServiceMock.mockImplementation(() => {
         return {
           ...useTestCaseServiceMockResolved,
