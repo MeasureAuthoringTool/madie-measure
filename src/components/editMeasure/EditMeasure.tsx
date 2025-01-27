fe848bc2e6be24e12dda1e4ee6d9a3ccc97c4c2b
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2025-01-27 13:14:53 -0800

    MAT-8171 tab style change

--- src/components/editMeasure/EditMeasure.tsx
+++ src/components/editMeasure/EditMeasure.tsx
@@ -419,7 +419,7 @@ export default function EditMeasure() {
   const isQDM = measure?.model?.includes("QDM");
   const contentDiv = (
     <div data-testid="editMeasure">
-      <div tw="relative" style={{ marginTop: "-60px" }}>
+      <div tw="relative" style={{ marginTop: "-48px" }}>
         <EditMeasureNav isQDM={isQDM} />
         <div
           style={{
