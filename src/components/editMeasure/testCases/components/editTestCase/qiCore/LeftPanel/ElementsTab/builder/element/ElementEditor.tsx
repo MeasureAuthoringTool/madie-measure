952755d92811173367d732dd349240758c84d5ab
Christopher Hubert <christopher.hubert@semanticbits.com>
2025-01-31 17:30:06 -0500

    MAT-8152: move formik generation up

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/ElementEditor.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/ElementEditor.tsx
@@ -23,7 +23,6 @@ interface ElementEditorProps {
   onChange?: (path: string, value: any) => void;
   canEdit: boolean;
 }
-
 const ElementEditor = ({
   selectedResource,
   resource,
@@ -37,7 +36,6 @@ const ElementEditor = ({
   const [loading, setLoading] = useState(true);
   const [initialValues, setInitialValues] = useState({});
   const [validationSchema, setValidationSchema] = useState({});
-
   const buildNode = async (
     child,
     resourcePath,
@@ -68,11 +66,24 @@ const ElementEditor = ({
           return nodeList; // Return the aggregated node list
         }
       }
+      // This is the edge case for when we're providing the root of the structure like ClaimResponse as it's not a componentDataType and there is no type
+      const required = +child.min > 0;
+      const elemPath = child.path;
+      const value = _.get(resource, elemPath);
+      console.log("CHILD IS", child);
+      const builtNode = {
+        id: child?.id,
+        label: child.path.split(".").pop(),
+        value,
+        type,
+        required,
+        validation: null,
+      };
+      return nodeList.concat(builtNode);
     } else {
       // It's a single node. Add it to the node list
       const required = +child.min > 0;
       const elemPath = stripResourcePath(resourcePath, child.path);
-
       const value = _.get(resource, elemPath);
       const builtNode = {
         id: child?.id,
@@ -85,7 +96,6 @@ const ElementEditor = ({
       return nodeList.concat(builtNode);
     }
   };
-
   const buildForm = async (
     rootDefinition,
     allChildren,
@@ -112,7 +122,6 @@ const ElementEditor = ({
     }
     buildSchemaAndInitialValues(results);
   };
-
   const recursiveAddYupObject = (validationSchema) => {
     // Iterate over each key in the object
     for (const key in validationSchema) {
@@ -131,7 +140,6 @@ const ElementEditor = ({
     }
     return validationSchema;
   };
-
   // given form info, we're going to make an object of schemas and save it to state for formik.
   const buildSchemaAndInitialValues = (formInfo) => {
     const initialValuesObject = {};
@@ -161,12 +169,11 @@ const ElementEditor = ({
     // need a loading toggle or formikProvider dies violently.
     setLoading(false);
   };
-
   const triggerFormBuilder = async () => {
-    const currentPath = elementDefinition?.path;
+    const currentPath = selectedResource.definition.type;
     const allChildren = getAllChildren(selectedResource, currentPath);
     await buildForm(
-      elementDefinition,
+      selectedResource?.definition?.snapshot?.element?.[0],
       allChildren,
       fhirDefinitionsService,
       resourcePath,
@@ -174,17 +181,18 @@ const ElementEditor = ({
     );
   };
   useEffect(() => {
-    if (elementDefinition) {
+    if (selectedResource) {
+      console.log("elementDefinittion", elementDefinition);
       triggerFormBuilder();
     }
-  }, [elementDefinition?.id]);
-
+  }, [selectedResource]);
   const formik = useFormik({
     initialValues,
     enableReinitialize: true,
     validationSchema,
     onSubmit: (values) => {},
   });
+  console.log("formik", formik);
   if (_.isNil(elementDefinition)) {
     return <span>No element selected</span>;
   }
@@ -223,5 +231,4 @@ const ElementEditor = ({
   }
   return <div />;
 };
-
 export default ElementEditor;
