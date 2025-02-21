37777b63a1f6db2822688b0a43d750b3588e1534
mcmcphillips <matt.mcphillips@icf.com>
2025-02-21 08:39:34 -0800

    MAT-8128: Revert changes to see if it stops breaking in the pipeline

--- src/components/editMeasure/testCases/__mocks__/dayjs.js
+++ src/components/editMeasure/testCases/__mocks__/dayjs.js
@@ -1,11 +1,7 @@
 const mock = jest.genMockFromModule("dayjs");
 const utc = jest.requireActual("dayjs/plugin/utc");
-const timezone = jest.requireActual("dayjs/plugin/timezone");
 const dayjs = jest.requireActual("dayjs");
-const advancedFormat = jest.requireActual("dayjs/plugin/advancedFormat");
 
 dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(advancedFormat);
 
 module.exports = dayjs;
