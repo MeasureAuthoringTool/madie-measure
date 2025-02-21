0ca5a01e7e6dd59cc968c016ab3c7d92b5a714ff
sb-cecilialiu <cecilia.liu@icf.com>
2025-02-21 10:03:10 -0600

    MAT-8236 change data test id

--- src/components/editMeasure/details/MeasureDefinitions/MeasureDefinitions.tsx
+++ src/components/editMeasure/details/MeasureDefinitions/MeasureDefinitions.tsx
@@ -354,7 +354,7 @@ const MeasureDefinitions = (props: MeasureDefinitionsProps) => {
                     startAdornment: (
                       <InputAdornment
                         position="start"
-                        data-testid="test-cases-trigger-search"
+                        data-testid="measure-definition-search"
                         onClick={handleNavigate}
                         style={{ cursor: "pointer" }}
                       >
