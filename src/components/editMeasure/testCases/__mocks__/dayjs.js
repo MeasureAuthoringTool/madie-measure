001f42ea6874aed97e6f6d5b7ef80baa00abc07e
mcmcphillips <matt.mcphillips@icf.com>
2025-02-21 09:26:47 -0800

    MAT-8128: Force tz in tests. Not sure if this fixes the issues

--- src/components/editMeasure/testCases/__mocks__/dayjs.js
+++ src/components/editMeasure/testCases/__mocks__/dayjs.js
@@ -1,7 +1,12 @@
 const mock = jest.genMockFromModule("dayjs");
 const utc = jest.requireActual("dayjs/plugin/utc");
+const timezone = jest.requireActual("dayjs/plugin/timezone");
 const dayjs = jest.requireActual("dayjs");
 
 dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("America/Los_Angeles");
 
+// Force dayjs to use Los Angeles time in tests
+dayjs.tz.setDefault("America/Los_Angeles");
 module.exports = dayjs;
