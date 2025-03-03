e5288928bb14270768bb18aaa84a2f2b89d49dbe
Greg Akins <gregory.akins@icf.com>
2025-03-03 10:50:36 -0500

    MAT-8269: A bit of refactoring

--- src/components/editMeasure/testCases/components/statusHandler/StatusHandler.tsx
+++ src/components/editMeasure/testCases/components/statusHandler/StatusHandler.tsx
@@ -6,6 +6,11 @@ import { TestCaseImportOutcome } from "@madie/madie-models";
 import "twin.macro";
 import "styled-components/macro";
 import { EXPORT_ERROR_CHARACTERS_MESSAGE } from "../../util/checkSpecialCharacters";
+import {
+  createImportMessage,
+  createWarningMessage,
+} from "./StatusHandlerMessage";
+
 interface StatusHandlerProps {
   error?: boolean;
   warning?: boolean;
@@ -94,29 +99,7 @@ const StatusHandler = ({
     const withoutDuplicates = [...new Set(warningMessages)];
 
     if (withoutDuplicates.length > 0) {
-      return (
-        <div id="status-handler">
-          <MadieAlert
-            data-testid={testDataId}
-            type="warning"
-            copyButton="true"
-            content={
-              <div aria-live="polite" role="alert" data-testid={testDataId}>
-                <div data-testid="warn-title">
-                  The following Test Case dates could not be shifted. Please try
-                  again. If the issue continues, please contact helpdesk.
-                  <ul>
-                    {withoutDuplicates.map((tc) => (
-                      <li>{tc}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            }
-            canClose={false}
-          />
-        </div>
-      );
+      return createWarningMessage(withoutDuplicates, testDataId);
     }
   }
   if (importWarnings && importWarnings.length > 0) {
@@ -128,70 +111,11 @@ const StatusHandler = ({
     const successfulImportsWithWarnings = importWarnings.filter((warnings) => {
       if (warnings.successful && warnings.message) return warnings;
     });
-    return (
-      <div id="status-handler">
-        <MadieAlert
-          copyButton="true"
-          type="warning"
-          content={
-            <div aria-live="polite" role="alert" data-testid={testDataId}>
-              {failedImports.length > 0 && (
-                <div>
-                  <div tw="font-medium">
-                    ({successfulImports}) test case(s) were imported. The
-                    following ({failedImports.length}) test case(s) could not be
-                    imported. Please ensure that your formatting is correct and
-                    try again.
-                  </div>
-                  <ul>
-                    {failedImports.map((failedImport) => {
-                      const family = failedImport?.familyName;
-                      const given = failedImport?.givenNames?.toString();
-                      const names =
-                        family && given
-                          ? `${family} ${given}`
-                          : failedImport?.patientId;
-                      return (
-                        <li data-testid="failed-test-cases">
-                          {names} <br />
-                          <span tw="ml-4">Reason: {failedImport.message}</span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              )}
-              {successfulImportsWithWarnings?.length > 0 && (
-                <div>
-                  <div tw="font-medium">
-                    Following test case(s) were imported successfully, but{" "}
-                    {successfulImportsWithWarnings[0].message}
-                  </div>
-                  <ul>
-                    {successfulImportsWithWarnings.map(
-                      (successfulImportsWithWarning) => {
-                        const family = successfulImportsWithWarning?.familyName;
-                        const given =
-                          successfulImportsWithWarning?.givenNames?.toString();
-                        const names =
-                          family && given
-                            ? `${family} ${given}`
-                            : successfulImportsWithWarning?.patientId;
-                        return (
-                          <li data-testid="success-imports-with-warnings">
-                            {names}{" "}
-                          </li>
-                        );
-                      }
-                    )}
-                  </ul>
-                </div>
-              )}
-            </div>
-          }
-          canClose={false}
-        />
-      </div>
+    return createImportMessage(
+      failedImports,
+      successfulImports,
+      successfulImportsWithWarnings,
+      testDataId
     );
   }
   return <div />;
---
+++ src/components/editMeasure/testCases/components/statusHandler/StatusHandlerMessage.test.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import StatusHandler from "./StatusHandler";
+import { EXPORT_ERROR_CHARACTERS_MESSAGE } from "../../util/checkSpecialCharacters";
+import {
+  createImportMessage,
+  createWarningMessage,
+} from "./StatusHandlerMessage";
+import { TestCaseImportOutcome } from "@madie/madie-models";
+
+describe("StatusHandler Messages", () => {
+  it("Creates a message", () => {
+    const testOutcomes: TestCaseImportOutcome[] = [
+      {
+        familyName: "Judith",
+        givenNames: ["Raoul", "Beth"],
+        patientId: "666",
+        message: "You're a message",
+        successful: false,
+      } as unknown as TestCaseImportOutcome,
+    ];
+    render(createImportMessage([], 1, testOutcomes, "test"));
+    const rootElement = screen.getByTestId("test");
+    expect(rootElement).toBeInTheDocument();
+    expect(
+      screen.getByTestId("success-imports-with-warnings")
+    ).toBeInTheDocument();
+  });
+});
