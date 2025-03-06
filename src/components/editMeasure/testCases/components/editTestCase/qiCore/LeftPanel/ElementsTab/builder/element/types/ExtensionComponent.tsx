0fe792af90769294c60e279ba73d5ed477a31e16
Greg Akins <gregory.akins@icf.com>
2025-03-06 11:10:31 -0500

    MAT-8296: Updating ExtensionComponent to show text only for read-only URLS

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/ExtensionComponent.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/ExtensionComponent.tsx
@@ -6,7 +6,7 @@ import {
 } from "fhir/r4";
 import UriComponent from "./UriComponent";
 import { Select } from "@madie/madie-design-system/dist/react";
-import { MenuItem } from "@mui/material";
+import { MenuItem, Typography } from "@mui/material";
 import TypeEditor from "../TypeEditor";
 import { StructureDefinitionDto } from "../../../../../../../../api/models/StructureDefinitionDto";
 
@@ -68,55 +68,67 @@ const ExtensionComponent = ({
   }, [selectedValueType, url, value]);
 
   const idPrefix = elementDefinition?.id?.split("Extension.").pop();
-
-  return (
-    <div data-testid={idPrefix}>
-      <UriComponent
-        canEdit={!urlElement?.fixedUri} // disable if this is fixed value
-        fieldRequired={urlElement?.min > 0}
-        label="url"
-        value={urlElement?.fixedUri}
-        structureDefinition={null}
-        onChange={(value) => setUrl(value)}
-      />
-      <Select
-        label="Value[x]"
-        inputProps={{
-          "data-testid": `${idPrefix}-type-selector-input`,
-        }}
-        data-testid={`${idPrefix}-type-selector`}
-        SelectDisplayProps={{
-          "aria-required": "true",
-        }}
-        disabled={false}
-        required={valueElement?.min > 0}
-        options={[
-          <MenuItem
-            key={selectedValueType}
-            value={selectedValueType}
-            data-testid={`type-option-${selectedValueType}`}
-          >
-            {selectedValueType}
-          </MenuItem>,
-        ]}
-        value={selectedValueType}
-        onChange={(e) => setSelectedValueType(e.target.value)}
-      />
-      {selectedValueType && (
-        <TypeEditor
-          structureDefinition={valueElement}
-          resource={fhirResource}
-          type={valueElement?.type[0]?.code}
-          required={valueElement.min > 0}
-          value={elementDefinition?.fixedUri}
-          onChange={(value) => setValue(value)} // do nothing for now
-          canEdit={canEdit}
-          label={`value${selectedValueType}`}
-          parentStructureDefinition={elementDefinition}
+  if (urlElement?.fixedUri) {
+    return (
+      <>
+        <Typography data-testid={idPrefix} sx={{ fontSize: "14px" }}>
+          <span style={{ color: "#1976d2", fontWeight: 700 }}>
+            {urlElement?.fixedUri}
+          </span>
+          <br />
+          <span style={{ color: "#333333" }}>{urlElement?.fixedUri}</span>
+        </Typography>
+      </>
+    );
+  } else
+    return (
+      <div data-testid={idPrefix}>
+        <UriComponent
+          canEdit={!urlElement?.fixedUri} // disable if this is fixed value
+          fieldRequired={urlElement?.min > 0}
+          label="url"
+          value={urlElement?.fixedUri}
+          structureDefinition={null}
+          onChange={(value) => setUrl(value)}
         />
-      )}
-    </div>
-  );
+        <Select
+          label="Value[x]"
+          inputProps={{
+            "data-testid": `${idPrefix}-type-selector-input`,
+          }}
+          data-testid={`${idPrefix}-type-selector`}
+          SelectDisplayProps={{
+            "aria-required": "true",
+          }}
+          disabled={false}
+          required={valueElement?.min > 0}
+          options={[
+            <MenuItem
+              key={selectedValueType}
+              value={selectedValueType}
+              data-testid={`type-option-${selectedValueType}`}
+            >
+              {selectedValueType}
+            </MenuItem>,
+          ]}
+          value={selectedValueType}
+          onChange={(e) => setSelectedValueType(e.target.value)}
+        />
+        {selectedValueType && (
+          <TypeEditor
+            structureDefinition={valueElement}
+            resource={fhirResource}
+            type={valueElement?.type[0]?.code}
+            required={valueElement.min > 0}
+            value={elementDefinition?.fixedUri}
+            onChange={(value) => setValue(value)} // do nothing for now
+            canEdit={canEdit}
+            label={`value${selectedValueType}`}
+            parentStructureDefinition={elementDefinition}
+          />
+        )}
+      </div>
+    );
 };
 
 export default ExtensionComponent;
