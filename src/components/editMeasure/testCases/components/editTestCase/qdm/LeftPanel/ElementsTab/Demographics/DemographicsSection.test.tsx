d45a0a773f430c945d3a9c3a13f8fb2af55e0523
adongare <ashok.dongare@semanticbits.com>
2025-03-17 15:12:26 -0400

    MAT-8367 fix test cases

--- src/components/editMeasure/testCases/components/editTestCase/qdm/LeftPanel/ElementsTab/Demographics/DemographicsSection.test.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qdm/LeftPanel/ElementsTab/Demographics/DemographicsSection.test.tsx
@@ -7,6 +7,7 @@ import {
   useQdmPatient,
 } from "../../../../../../util/QdmPatientContext";
 import { QdmExecutionContextProvider } from "../../../../../routes/qdm/QdmExecutionContext";
+import { demographicValueSets } from "../../../../../../__mocks__/demographicValueSets";
 import {
   QDMPatient,
   PatientCharacteristicEthnicity,
@@ -74,68 +75,7 @@ const cqmMeasure = {
     { qdmStatus: "ethnicity", codeListId: "2.16.840.1.114222.4.11.837" },
     { qdmStatus: "gender", codeListId: "2.16.840.1.113762.1.4.1021.121" },
   ],
-  value_sets: [
-    {
-      display_name: "Race",
-      oid: "2.16.840.1.114222.4.11.836",
-      concepts: [
-        {
-          code: "1002-5",
-          code_system_name: "CDCREC",
-          code_system_oid: "2.16.840.1.113883.6.238",
-          code_system_version: "1.2",
-          display_name: "American Indian or Alaska Native",
-        },
-        {
-          code: "2028-9",
-          code_system_name: "CDCREC",
-          code_system_oid: "2.16.840.1.113883.6.238",
-          code_system_version: "1.2",
-          display_name: "Asian",
-        },
-      ],
-    },
-    {
-      display_name: "Ethnicity",
-      oid: "2.16.840.1.114222.4.11.837",
-      concepts: [
-        {
-          code: "2135-2",
-          code_system_name: "CDCREC",
-          code_system_oid: "2.16.840.1.113883.6.238",
-          code_system_version: "1.2",
-          display_name: "Hispanic or Latino",
-        },
-        {
-          code: "2186-5",
-          code_system_name: "CDCREC",
-          code_system_oid: "2.16.840.1.113883.6.238",
-          code_system_version: "1.2",
-          display_name: "Not Hispanic or Latino",
-        },
-      ],
-    },
-    {
-      display_name: "CMSSex",
-      oid: "2.16.840.1.113762.1.4.1021.121",
-      concepts: [
-        {
-          code: "248152002",
-          code_system_name: "SNOMEDCT",
-          code_system_oid: "2.16.840.1.113883.6.96",
-          code_system_version: "2024-09",
-          display_name: "Female (finding)",
-        },
-        {
-          code: "248153007",
-          code_system_name: "SNOMEDCT",
-          code_system_oid: "2.16.840.1.113883.6.96",
-          code_system_version: "2024-09",
-          display_name: "Male (finding)",
-        },
-      ],
-    },
-  ],
+  value_sets: demographicValueSets,
 };
 
 describe("DemographicsSection", () => {
