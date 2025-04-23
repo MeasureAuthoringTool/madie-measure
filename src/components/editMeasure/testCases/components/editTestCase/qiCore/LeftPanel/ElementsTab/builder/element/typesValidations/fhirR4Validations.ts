70fcb568562931b8c84f50091be9145062fbe326
Greg Akins <gregory.akins@icf.com>
2025-04-23 10:43:42 -0400

    MAT-8231: Finalizing error message and correct RegEx behavior

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/typesValidations/fhirR4Validations.ts
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/typesValidations/fhirR4Validations.ts
@@ -139,11 +139,11 @@ export const getDecimalValidator = (required) => {
 
 export const getUriValidator = (required) => {
   const uriRegex: RegExp = /\S*/;
-  const urnRegex: RegExp = /urn:oid:[0-2](\.(0|[1-9][0-9]*))+/;
+  const urnRegex: RegExp = /^urn:oid:[0-2](\.(0|[1-9][0-9]*))+$/;
   const baseValidator = Yup.string()
     .test(
       "urn-specific-test",
-      "Invalid OID Format (ie., urn:oid:1.2.36.146.595.217.0.1 ).",
+      "Invalid OID Format (example format: urn:oid:1.2.36.146.595.217.0.1 ).",
       function (value) {
         if (value && value.startsWith("urn:oid")) {
           return urnRegex.test(value);
