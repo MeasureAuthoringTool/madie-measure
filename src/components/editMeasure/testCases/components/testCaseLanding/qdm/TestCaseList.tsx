9cc37629ca89fa712eb2b550d1a976c8e94067b7
Prateek Keerthi <prateekkeerthi7@gmail.com>
2025-05-15 13:33:51 -0400

    MAT-8605 allow creating new test cases for qdm measures

--- src/components/editMeasure/testCases/components/testCaseLanding/qdm/TestCaseList.tsx
+++ src/components/editMeasure/testCases/components/testCaseLanding/qdm/TestCaseList.tsx
@@ -239,7 +239,8 @@ const TestCaseList = (props: TestCaseListProps) => {
       checkUserCanEdit(
         measure?.measureSet?.owner,
         measure?.measureSet?.acls,
-        measure?.measureMetaData?.draft
+        measure?.measureMetaData?.draft,
+        featureFlags.EditTestsOnVersionedMeasures
       )
     );
   }, [measure]);
