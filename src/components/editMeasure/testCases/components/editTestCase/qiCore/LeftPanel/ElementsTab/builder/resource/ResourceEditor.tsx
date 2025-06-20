e39313c1e6691f17e5514350eb370ff2370241c5
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2025-06-20 11:33:52 -0700

    MAT-8685 add a profile to attribute edit screen

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/resource/ResourceEditor.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/resource/ResourceEditor.tsx
@@ -267,6 +267,20 @@ const ResourceEditor = ({
               <span style={{ color: "#333333" }}>
                 {selectedResource?.bundleEntry?.resource?.id}
               </span>
+                            <div />
+              <span style={{ color: "125496", fontWeight: 700 }}>
+                Profile:&nbsp;&nbsp;
+              </span>
+              <span style={{ color: "#333333" }}>
+                {console.log(
+                  selectedResource?.bundleEntry?.resource?.meta?.profile
+                )}
+                {selectedResource?.bundleEntry?.resource?.meta?.profile?.[0]
+                  ?.match(/\/(qicore|core)\//)
+                  .includes("qicore")
+                  ? "QICore"
+                  : "US Core"}
+              </span>
             </Typography>
             <IconButton
               data-testid="close-resource-editor-button"
