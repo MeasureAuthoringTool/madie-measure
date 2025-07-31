19c0bb88a9af8308e4648d085975e009a0eca59d
sb-cecilialiu <cecilia.liu@icf.com>
2025-07-31 06:14:59 -0500

    MAT-8774 remove extension from side nav

--- src/components/editMeasure/testCases/api/fhirDefinitionServiceUtilities.tsx
+++ src/components/editMeasure/testCases/api/fhirDefinitionServiceUtilities.tsx
@@ -280,6 +280,7 @@ export function getTopLevelElements(resource: any) {
     (e) =>
       e.path.split(".")?.length === 2 &&
       e.id !== "Extension.extension" &&
+      e.id !== "Patient.extension" &&
       e.max !== "0" &&
       // Exclude entries where the path contains".contained",".text",".meta",".language",".implicitRules"
       ![".contained", ".text", ".meta", ".language", ".implicitRules"].some(
@@ -466,7 +467,7 @@ export function mergePathWithIndex(pathWithIndex, pathWithoutIndex) {
 }
 
 export function addCardinalityToElement(nextEntry, elemPath) {
-  if (!nextEntry.resource[elemPath]) {
+  if (!nextEntry?.resource[elemPath]) {
     // make it accessible to avoid a null
     nextEntry.resource[elemPath] = {};
   }
