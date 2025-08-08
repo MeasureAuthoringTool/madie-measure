e5b7cc852daee439a3a5a0661cfd325c3d1133a5
mcmcphillips <matt.mcphillips@icf.com>
2025-08-08 13:05:54 -0700

    MAT-8870-POC

--- src/components/editMeasure/EditMeasureNav.tsx
+++ src/components/editMeasure/EditMeasureNav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   NavLink,
   useLocation,
@@ -9,16 +9,52 @@ import {
 import tw, { styled } from "twin.macro";
 import { Tabs, Tab } from "@madie/madie-design-system/dist/react";
 import { measureStore } from "@madie/madie-util";
+import Joyride, {
+  ACTIONS,
+  CallBackProps,
+  EVENTS,
+  STATUS,
+  Step,
+} from "react-joyride";
+import { TooltipRenderProps } from "react-joyride";
+
 interface PropTypes {
   isActive?: boolean;
 }
 
-const MenuItemContainer = tw.ul`bg-transparent flex px-8`;
-
-const MenuItem = styled.li((props: PropTypes) => [
-  tw`mr-1 text-white bg-[rgba(0, 32, 64, 0.5)] rounded-t-md hover:bg-[rgba(0,6,13, 0.5)]`,
-  props.isActive && tw`bg-slate text-slate-90 font-medium hover:bg-slate`,
-]);
+const MyCustomTooltip = ({
+  step,
+  backProps,
+  closeProps,
+  primaryProps,
+  tooltipProps,
+}: TooltipRenderProps) => {
+  return (
+    <div
+      {...tooltipProps}
+      style={{
+        padding: 20,
+        background: "#222",
+        color: "#fff",
+        borderRadius: 8,
+      }}
+    >
+      <h4 style={{ marginTop: 0 }}>{step.title}</h4>
+      <div>{step.content}</div>
+      <div
+        style={{
+          marginTop: 12,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <button {...backProps}>Back</button>
+        <button {...primaryProps}>Next</button>
+        <button {...closeProps}>X</button>
+      </div>
+    </div>
+  );
+};
 
 const EditMeasureNav = ({ isQDM }) => {
   const [testCaseLength, setTestCaseLength] = useState<any>(null);
@@ -57,14 +93,178 @@ const EditMeasureNav = ({ isQDM }) => {
       }
     }
   }, [measureId, measure?.testCases, measure?.testCases?.length]);
+
+  //
+  const steps = [
+    {
+      content: (
+        <div>
+          <b>Details:</b> The details tab is where users can enter all the meta
+          information for the measure.
+        </div>
+      ),
+      placement: "bottom",
+      target: "#measure-details-tab",
+      disableBeacon: true, // comment out to see a little visual indicator
+    },
+    {
+      content: (
+        <div>
+          <b>CQL:</b> The CQL tab is where users can enter the measures CQL. It
+          contains a free text editor as well as UI builder tabs allowing the
+          user to dynamically create the measure CQL.
+        </div>
+      ),
+      placement: "bottom",
+      target: "#cql-editor-tab",
+    },
+    {
+      content: (
+        <div>
+          <b>Population Criteria:</b> This tab is where the populations are set
+          up, The user will select measure types, Scoring type, and connect the
+          CQL to populations.
+        </div>
+      ),
+      placement: "bottom",
+      target: "#groups-tab",
+    },
+    {
+      content: (
+        <div>
+          <b>Test Cases:</b> This tab is where users will create synthetic
+          patients that are designed to test the measures CQL.
+        </div>
+      ),
+      placement: "bottom",
+      target: "#patients-tab",
+    },
+  ];
+  const [joyRideState, setJoyRideState] = useState({
+    run: false,
+    stepIndex: 0,
+    steps,
+  });
+
+  // uncomment this guy to run the stuff the joyride on load
+  // useLayoutEffect(() => {
+  //   setJoyRideState((prevState) => ({
+  //       ...prevState,
+  //       run: true
+  //   }))
+  // }, [])
+
+  // progrematic stepping example that we can hook into any button click.
+  //  Just need to do an additional check to see whether we're currently in a joyride, such as
+  // if joyRideState.run === true, then invoke toggleNext step, and add to each button logic click
+
+  const toggleNextStep = (e, v) => {
+    console.log("e", e);
+    console.log("v", v);
+    setJoyRideState((prevState) => {
+      // If stepIndex is currently the last step (steps.length - 1),
+      // reset to 0 and stop running
+      if (prevState.stepIndex >= prevState.steps.length - 1) {
+        return {
+          ...prevState,
+          stepIndex: 0,
+          run: false,
+        };
+      }
+      // Otherwise, go to the next step
+      return {
+        ...prevState,
+        stepIndex: prevState.stepIndex + 1,
+      };
+    });
+  };
+
+  const handleJoyrideCallback = (data: CallBackProps) => {
+    const { action, index, status, type } = data;
+
+    if (
+      ([EVENTS.STEP_AFTER, EVENTS.TARGET_NOT_FOUND] as string[]).includes(type)
+    ) {
+      // Update state to advance the tour
+      setJoyRideState((prevState) => ({
+        ...prevState,
+        stepIndex: index + (action === ACTIONS.PREV ? -1 : 1),
+      }));
+      // setState({ stepIndex: index + (action === ACTIONS.PREV ? -1 : 1) });
+    } else if (
+      ([STATUS.FINISHED, STATUS.SKIPPED] as string[]).includes(status)
+    ) {
+      // Need to set our running state to false, so we can restart if we click start again.
+      // setState({ run: false });
+      setJoyRideState((prevState) => ({ ...prevState, run: false }));
+    }
+  };
+
   return (
     <div>
+      <button
+        onClick={() => {
+          console.log("joyrideState", joyRideState);
+          setJoyRideState((prevState) => ({ ...prevState, run: true }));
+        }}
+      >
+        start joyride
+      </button>
+      {/* @ts-ignore */}
+      <Joyride
+        steps={joyRideState?.steps}
+        stepIndex={joyRideState?.stepIndex}
+        continuous
+        run={joyRideState?.run}
+        scrollToFirstStep
+        showSkipButton
+        callback={handleJoyrideCallback}
+        spotlightClicks // make this truthy to allow clicking through the overlay, effectively forcing user to interract
+        // tooltipComponent={MyCustomTooltip} // full override of the component that shows up
+
+        // very mui 5.0 useStyles like feel to a style component. Use this OR just make your own entire reusable component. See above example myCustomTooltip. We should really do that one.
+        // styles={{
+        // options: {
+        //   zIndex: 10000,             // always on top
+        //   primaryColor: '#0055aa',   // changes the "Next" button & spotlight
+        //   textColor: '#222',         // default text color
+        //   backgroundColor: '#fffbe6',// background of the tooltip
+        // },
+
+        // main div thing
+        // tooltip: {
+        //   borderRadius: '12px',      // round corners
+        //   fontSize: '14px',
+        //   padding: '16px',
+        //   boxShadow: '0 4px 10px rgba(0,0,0,0.2)',
+        // },
+
+        // title prop
+        // tooltipTitle: {
+        //   fontWeight: 'bold',
+        //   fontSize: '16px',
+        //   marginBottom: '8px',
+        // },
+
+        // nextButton
+        // buttonNext: {
+        //   backgroundColor: '#28a745',
+        //   color: '#fff',
+        //   fontWeight: 'bold',
+        // },
+        // buttonBack: {
+        //   color: '#999',
+        //   marginRight: 8,
+        // },
+        // }}
+      />
       <div style={{ marginLeft: "32px" }} id="edit-measure-nav-a">
-        <Tabs value={match} type="A" size="standard">
+        <Tabs value={match} type="A" size="standard" onChange={toggleNextStep}>
           <Tab
             value={`details`}
             to="details"
             data-testid="measure-details-tab"
+            id="measure-details-tab"
             type="A"
             size="standard"
             label="Details"
@@ -73,6 +273,7 @@ const EditMeasureNav = ({ isQDM }) => {
           <Tab
             value="cql-editor"
             to={`cql-editor`}
+            id="cql-editor-tab"
             data-testid="cql-editor-tab"
             type="A"
             size="standard"
@@ -82,6 +283,7 @@ const EditMeasureNav = ({ isQDM }) => {
           <Tab
             value={isQDM ? `base-configuration` : `groups`}
             to={isQDM ? `base-configuration` : `groups/1`}
+            id="groups-tab"
             data-testid="groups-tab"
             type="A"
             size="standard"
@@ -92,6 +294,7 @@ const EditMeasureNav = ({ isQDM }) => {
             value={`test-cases`}
             to={`test-cases/list-page`}
             data-testid="patients-tab"
+            id="patients-tab"
             type="A"
             size="standard"
             label={testCaseLabel}
