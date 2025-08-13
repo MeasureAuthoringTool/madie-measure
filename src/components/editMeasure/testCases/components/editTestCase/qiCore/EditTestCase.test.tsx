9a9c5bddbabafd1ec562b6ef8fb32b07c8c88cc2
Christopher Hubert <christopher.hubert@semanticbits.com>
2025-08-13 10:15:54 -0400

    MAT-8892: add tests

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/EditTestCase.test.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/EditTestCase.test.tsx
@@ -888,6 +888,176 @@ describe("EditTestCase component", () => {
       );
     });
 
+    it("should alert for updated datetime without hours", async () => {
+      const testCase = {
+        id: "1234",
+        description: "Test IPP",
+        series: "SeriesA",
+        createdBy: MEASURE_CREATEDBY,
+        createdAt: "",
+        lastModifiedAt: "",
+        lastModifiedBy: "null",
+        title: "TestIPP",
+        name: "TestIPP",
+        executionStatus: "false",
+        json: null,
+      } as TestCase;
+      mockedAxios.get.mockClear().mockImplementation((args) => {
+        if (args && args.endsWith("series")) {
+          return Promise.resolve({ data: [] });
+        } else if (args && args.endsWith("resources")) {
+          return Promise.resolve({
+            data: [...resourceIdentifiers],
+          });
+        }
+        return Promise.resolve({
+          data: testCase,
+        });
+      });
+      renderWithRouter(
+        ["/measures/m1234/edit/test-cases/1234"],
+        "/measures/:measureId/edit/test-cases/:id"
+      );
+
+      const testCaseDescription = "TestCase123";
+      const testCaseTitle = "TestTitle";
+      const testCaseJson = JSON.stringify({
+        resourceType: "Bundle",
+        id: "43",
+        date: "2025-10-06T",
+      });
+
+      mockedAxios.put.mockResolvedValue({
+        data: {
+          ...testCase,
+          createdBy: MEASURE_CREATEDBY,
+          description: testCaseDescription,
+          title: testCaseTitle,
+          json: testCaseJson,
+          hapiOperationOutcome: {
+            code: 500,
+            message: "An unknown error occurred with HAPI FHIR",
+            outcomeResponse: {
+              resourceType: "OperationOutcome",
+              text: "Error: Bad things happened",
+              issue: [
+                {
+                  severity: "error",
+                  diagnostics: "Bad things happened",
+                },
+              ],
+            },
+          },
+        },
+      });
+
+      const editor = screen.getByTestId("test-case-json-editor");
+      await waitFor(() => expect(editor).toHaveValue(""));
+      userEvent.paste(editor, testCaseJson);
+      expect(editor).toHaveValue(testCaseJson);
+      userEvent.click(screen.getByTestId("details-tab"));
+
+      await testTitle("TC1", true);
+
+      const createBtn = await screen.findByRole("button", {
+        name: "Save",
+      });
+      userEvent.click(createBtn);
+
+      const debugOutput = await screen.findByTestId("error-toast");
+      expect(debugOutput).toHaveTextContent(
+        "Test case updated successfully with errors in JSON"
+      );
+      expect(debugOutput).toHaveTextContent(
+        "Timezone offsets have been added when hours are present, otherwise timezone offsets are removed or set to UTC for consistency."
+      );
+    });
+
+    it("should alert for updated datetime with only hours", async () => {
+      const testCase = {
+        id: "1234",
+        description: "Test IPP",
+        series: "SeriesA",
+        createdBy: MEASURE_CREATEDBY,
+        createdAt: "",
+        lastModifiedAt: "",
+        lastModifiedBy: "null",
+        title: "TestIPP",
+        name: "TestIPP",
+        executionStatus: "false",
+        json: null,
+      } as TestCase;
+      mockedAxios.get.mockClear().mockImplementation((args) => {
+        if (args && args.endsWith("series")) {
+          return Promise.resolve({ data: [] });
+        } else if (args && args.endsWith("resources")) {
+          return Promise.resolve({
+            data: [...resourceIdentifiers],
+          });
+        }
+        return Promise.resolve({
+          data: testCase,
+        });
+      });
+      renderWithRouter(
+        ["/measures/m1234/edit/test-cases/1234"],
+        "/measures/:measureId/edit/test-cases/:id"
+      );
+
+      const testCaseDescription = "TestCase123";
+      const testCaseTitle = "TestTitle";
+      const testCaseJson = JSON.stringify({
+        resourceType: "Bundle",
+        id: "43",
+        date: "2025-10-06T12",
+      });
+
+      mockedAxios.put.mockResolvedValue({
+        data: {
+          ...testCase,
+          createdBy: MEASURE_CREATEDBY,
+          description: testCaseDescription,
+          title: testCaseTitle,
+          json: testCaseJson,
+          hapiOperationOutcome: {
+            code: 500,
+            message: "An unknown error occurred with HAPI FHIR",
+            outcomeResponse: {
+              resourceType: "OperationOutcome",
+              text: "Error: Bad things happened",
+              issue: [
+                {
+                  severity: "error",
+                  diagnostics: "Bad things happened",
+                },
+              ],
+            },
+          },
+        },
+      });
+
+      const editor = screen.getByTestId("test-case-json-editor");
+      await waitFor(() => expect(editor).toHaveValue(""));
+      userEvent.paste(editor, testCaseJson);
+      expect(editor).toHaveValue(testCaseJson);
+      userEvent.click(screen.getByTestId("details-tab"));
+
+      await testTitle("TC1", true);
+
+      const createBtn = await screen.findByRole("button", {
+        name: "Save",
+      });
+      userEvent.click(createBtn);
+
+      const debugOutput = await screen.findByTestId("error-toast");
+      expect(debugOutput).toHaveTextContent(
+        "Test case updated successfully with errors in JSON"
+      );
+      expect(debugOutput).toHaveTextContent(
+        "Timezone offsets have been added when hours are present, otherwise timezone offsets are removed or set to UTC for consistency."
+      );
+    });
+
     it("should alert for test validation status", async () => {
       const testCase = {
         id: "1234",
