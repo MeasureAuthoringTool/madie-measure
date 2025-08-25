0869b08cf4f946ce58a0f1fcd638ce4541514065
Rohit Kandimalla <rohit.kandimalla@icf.com>
2025-08-25 12:44:04 -0400

    MAT-9018 Modified export file names for measure, test cases and overlapping codes

--- src/utils/measureModelHelpers.ts
+++ src/utils/measureModelHelpers.ts
@@ -1,8 +1,7 @@
 export default function getModelFamily(model: string) {
   if (model) {
-    const modelVersion = model?.split(" ")[1]?.split(".")[0]?.charAt(1);
     return model && model.startsWith("QI-Core")
-      ? `FHIR${modelVersion}`
+      ? `FHIR`
       : `${model?.split(" ")[0]}`;
   }
 }
