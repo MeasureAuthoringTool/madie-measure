7f398ebe027c54f10400ea7c3edbce951c5cca0f
Christopher Hubert <christopher.hubert@semanticbits.com>
2025-08-28 14:23:43 -0400

    MAT-9022: Add Calculator Button

---
+++ src/components/editMeasure/testCases/components/editTestCase/calculator/EditorCalculator.test.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import EditorCalculator from "./EditorCalculator";
+
+describe("Test EditorCalculator component", () => {
+  it("should register the calculator event on clicking calculator button", async () => {
+    const eventListenerSpy = jest.fn();
+    window.addEventListener("toggleEditorCalculatorBox", eventListenerSpy);
+    render(<EditorCalculator />);
+    const searchButton = screen.getByRole("button");
+    userEvent.click(searchButton);
+    expect(eventListenerSpy).toHaveBeenCalled();
+  });
+});
