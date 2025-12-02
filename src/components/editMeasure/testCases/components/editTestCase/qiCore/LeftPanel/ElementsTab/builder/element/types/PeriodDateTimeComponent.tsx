dead5eff21208159f5241ed14f85222414552969
chubert-sb <85900348+chubert-sb@users.noreply.github.com>
2025-12-02 11:56:32 -0500

    MAT-9280: add pasting to date fields (#1263)
    * MAT-9280: add pasting to date fields
    
    * MAT-9280: handle invalid dates
    
    * MAT-9280: handle invalid dates

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/PeriodDateTimeComponent.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/PeriodDateTimeComponent.tsx
@@ -180,7 +180,21 @@ const PeriodDateTimeComponent = ({
           }}
           data-testid="date-div"
         >
-          <div style={{ display: "flex", flexDirection: "column", gap: 4 }}>
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: 4 }}
+            onPaste={(e) => {
+              const pastedValue = e.clipboardData.getData("text");
+              const parsedDate = dayjs.utc(pastedValue).startOf("day");
+              if (!parsedDate.isValid()) {
+                console.error("Invalid date format pasted:", pastedValue);
+                return;
+              }
+              onChange({
+                start: parsedDate.format(format),
+                end: endDate?.format(format) || "",
+              });
+            }}
+          >
             <DateField
               label="Start Date"
               required={fieldRequired}
@@ -244,7 +258,21 @@ const PeriodDateTimeComponent = ({
             )}
           </div>
           <span style={{ alignSelf: "center", padding: "0 8px" }}>To</span>
-          <div style={{ display: "flex", flexDirection: "column", gap: 4 }}>
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: 4 }}
+            onPaste={(e) => {
+              const pastedValue = e.clipboardData.getData("text");
+              const parsedDate = dayjs.utc(pastedValue).startOf("day");
+              if (!parsedDate.isValid()) {
+                console.error("Invalid date format pasted:", pastedValue);
+                return;
+              }
+              onChange({
+                start: startDate ? startDate.format(format) : "",
+                end: parsedDate.format(format),
+              });
+            }}
+          >
             <DateField
               label="End Date"
               required={fieldRequired}
