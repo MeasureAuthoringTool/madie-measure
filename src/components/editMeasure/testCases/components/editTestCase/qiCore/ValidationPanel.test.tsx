908003c8460a2ab3c2e9d9005c15d0f6ec07925c
sb-cecilialiu <cecilia.liu@icf.com>
2026-01-06 06:20:45 -0600

    MAT-9430 remove unused import

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/ValidationPanel.test.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/ValidationPanel.test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import ValidationPanel, { extractResourceId } from "./ValidationPanel";
 import {
   HapiOperationOutcome,
