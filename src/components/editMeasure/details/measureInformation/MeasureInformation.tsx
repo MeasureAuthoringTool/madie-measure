77356aa7f5ab11b878b8d06640457a5c47ed2657
Prateek Keerthi <prateekkeerthi7@gmail.com>
2026-02-05 15:58:55 -0500

    MAT-9508 fixed test cases

--- src/components/editMeasure/details/measureInformation/MeasureInformation.tsx
+++ src/components/editMeasure/details/measureInformation/MeasureInformation.tsx
@@ -98,23 +98,13 @@ export default function MeasureInformation(props: MeasureInformationProps) {
     }
   };
 
-  const getMeasureOwnerName = (response) => {
-    const names = [response?.firstName, response?.lastName]
-      .map((name) => name?.trim())
-      .filter(Boolean);
-    return names.length
-      ? names.join(" ")
-      : !_.isEmpty(response?.harpId)
-      ? response.harpId
-      : "-";
-  };
-
   useEffect(() => {
     if (measure?.measureSet?.owner) {
       userServiceApi
         .getOwnerDetails(measure?.measureSet?.owner)
         .then((response) => {
-          setMeasureOwner(getMeasureOwnerName(response));
+          const ownerName = `${response?.firstName} ${response?.lastName}`;
+          setMeasureOwner(ownerName);
         })
         .catch(() => {
           setMeasureOwner("-");
