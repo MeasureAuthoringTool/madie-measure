b1fdcee9909363f426e9a270b02e2ffe98d74f0d
Prateek Keerthi <prateekkeerthi7@gmail.com>
2026-02-17 13:02:51 -0500

    MAT-9579 fixed broken test cases

--- src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseLanding.test.tsx
+++ src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseLanding.test.tsx
@@ -6,11 +6,20 @@ import {
   ApiContextProvider,
   ServiceConfig,
 } from "../../../../../../api/ServiceContext";
-import { Measure, Model } from "@madie/madie-models";
+import {
+  GroupPopulation,
+  Measure,
+  MeasureScoring,
+  Model,
+  PopulationExpectedValue,
+  TestCase,
+  ValidationStatus,
+} from "@madie/madie-models";
 import { Bundle, ValueSet } from "fhir/r4";
 import { ExecutionContextProvider } from "../../routes/qiCore/ExecutionContext";
 // @ts-ignore
 import { checkUserCanEdit } from "@madie/madie-util";
+import useFhirDefinitionsServiceApi from "../../../api/useFhirDefinitionsService";
 
 const serviceConfig = {
   measureService: {
@@ -28,6 +37,67 @@ const serviceConfig = {
 } as ServiceConfig;
 
 const MEASURE_CREATEDBY = "testuser";
+
+const testCases = [
+  {
+    id: "1",
+    description: "Test IPP",
+    title: "WhenAllGood",
+    series: "IPP_Pass",
+    lastModifiedAt: "2024-09-10T09:56:14.382Z",
+    validResource: true,
+    groupPopulations: [
+      {
+        groupId: "1",
+        scoring: MeasureScoring.PROPORTION,
+        populationValues: [
+          {
+            name: "initialPopulation",
+            expected: true,
+          },
+          {
+            name: "denominator",
+            expected: false,
+          },
+          {
+            name: "numerator",
+            expected: true,
+          },
+        ] as PopulationExpectedValue[],
+      },
+    ] as GroupPopulation[],
+    validationStatus: ValidationStatus.VALID,
+  },
+  {
+    id: "2",
+    description: "Test IPP Fail when something is wrong",
+    title: "WhenSomethingIsWrong",
+    series: "IPP_Fail",
+    lastModifiedAt: "2024-09-10T09:57:14.382Z",
+    validResource: true,
+    groupPopulations: [
+      {
+        groupId: "1",
+        scoring: MeasureScoring.PROPORTION,
+        populationValues: [
+          {
+            name: "initialPopulation",
+            expected: false,
+          },
+          {
+            name: "denominator",
+            expected: false,
+          },
+          {
+            name: "numerator",
+            expected: true,
+          },
+        ] as PopulationExpectedValue[],
+      },
+    ] as GroupPopulation[],
+    validationStatus: ValidationStatus.VALID,
+  },
+] as TestCase[];
 
 const measure = {
   id: "m1234",
@@ -67,9 +137,24 @@ jest.mock("../common/copyTestCases/CopyTestCaseDialog", () => ({
     <div data-testid="copy-test-case-dialog">Copy Test Case Dialog</div>
   ),
 }));
+
+jest.mock("../../../api/useFhirDefinitionsService");
+const useFhirDefinitionsServiceMock = useFhirDefinitionsServiceApi as jest.Mock;
+
+const fhirDefinitionsServiceMockResolved = {
+  getTestCaseExecutionBundle: jest.fn().mockResolvedValue({
+    testCases,
+    modifiedTestCaseIds: testCases.map((tc) => tc.id),
+  }),
+} as unknown as ReturnType<typeof useFhirDefinitionsServiceApi>;
+
 describe("TestCaseLanding component", () => {
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  useFhirDefinitionsServiceMock.mockImplementation(() => {
+    return fhirDefinitionsServiceMockResolved;
   });
 
   function renderTestCaseLandingComponent(measure: Measure) {
