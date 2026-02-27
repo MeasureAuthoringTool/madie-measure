38d904c53777681daef27b42d4b7d81ac868b948
mcmcphillips <matt.mcphillips@semanticBits.com>
2026-02-27 11:21:32 -0800

    MAT-9533: Update scroll pattern measuresList and testcase list

--- src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseList.tsx
+++ src/components/editMeasure/testCases/components/testCaseLanding/qiCore/TestCaseList.tsx
@@ -25,7 +25,6 @@ import CreateCodeCoverageNavTabs from "./CreateCodeCoverageNavTabs";
 import CodeCoverageHighlighting from "../common/CodeCoverageHighlighting";
 import CreateNewTestCaseDialog from "../../createTestCase/CreateNewTestCaseDialog";
 import {
-  MadieDeleteDialog,
   MadieSpinner,
   Pagination,
   Toast,
@@ -673,7 +672,13 @@ const TestCaseList = (props: TestCaseListProps) => {
               "data-testid": "close-error-button",
             }}
           />
-          <div tw="lg:col-span-5 pl-2 pr-2">
+          <div
+            className="calc-vh"
+            tw="lg:col-span-5 pl-2 pr-2"
+            // sticky container lives here.
+            // any child between here and sticky target need overflow: visible.
+            style={{ overflow: "auto" }}
+          >
             <div data-testid="code-coverage-tabs">
               <CreateCodeCoverageNavTabs
                 activeTab={activeTab}
@@ -709,7 +714,10 @@ const TestCaseList = (props: TestCaseListProps) => {
               onSuccess={insertTestCases}
             />
             {activeTab === "passing" && (
-              <div tw="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div
+                tw="overflow-x-auto sm:-mx-6 lg:-mx-8"
+                style={{ overflow: "visible" }}
+              >
                 <div tw="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                   {!executing && (
                     <>
