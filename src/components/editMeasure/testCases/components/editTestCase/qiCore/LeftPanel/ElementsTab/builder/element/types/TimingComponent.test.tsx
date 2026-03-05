4d399b40cebdfb18118b9bc996464d7a09869927
Greg Akins <gregory.akins@icf.com>
2026-03-05 09:21:18 -0500

    MAT-9544: Reverting test fixes that were incidentally discovered (created tech debt MAT-9782)

--- src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/TimingComponent.test.tsx
+++ src/components/editMeasure/testCases/components/editTestCase/qiCore/LeftPanel/ElementsTab/builder/element/types/TimingComponent.test.tsx
@@ -651,7 +651,7 @@ describe("TimingComponent", () => {
 
     // Test Event (DateTimeComponent)
     const eventDeleteButton = await screen.findByTestId(
-      "delete-button-MedicationRequest.dosageInstruction[0].timing.event[0]"
+      "delete-button-Event[0]"
     );
     expect(eventDeleteButton).toBeInTheDocument();
     userEvent.click(eventDeleteButton);
@@ -705,14 +705,14 @@ describe("TimingComponent", () => {
     renderTimingComponent({
       initialValues: {
         "MedicationRequest.dosageInstruction[0].timing": {
-          event: ["2022-01-01"],
+          event: ["2022-01-01T10:30:00"],
         },
       },
     });
 
     // Find the delete button for the only Event element
     const eventDeleteButton = await screen.findByTestId(
-      "delete-button-MedicationRequest.dosageInstruction[0].timing.event[0]"
+      "delete-button-Event[0]"
     );
     expect(eventDeleteButton).toBeInTheDocument();
 
