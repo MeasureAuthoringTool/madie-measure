229311127f4dfcd2da03485d6fdca5f5f4a7dc86
Ethan Kaplan <47760891+ethankaplan@users.noreply.github.com>
2026-08-10 13:55:29 -0700

    MAT-10277 change action center icons and order (#1626)
    * MAT-10277 change action center icons and order
    
    * remove the allow thing

--- src/components/measureLanding/measureList/actionCenter/ActionCenter.tsx
+++ src/components/measureLanding/measureList/actionCenter/ActionCenter.tsx
@@ -199,6 +199,15 @@ export default function ActionCenter(props: PropTypes) {
     setIsSharedWithUser(isSelectedMeasuresSharedWithUser(measures));
   }, [measures]);
 
+  const PipeSeparator = () => (
+    <span
+      aria-hidden="true"
+      style={{ color: "#8C8C8C", display: "inline-flex", alignItems: "center" }}
+    >
+      |
+    </span>
+  );
+
   return (
     <div data-testid="action-center">
       <DeleteAction
@@ -222,7 +231,16 @@ export default function ActionCenter(props: PropTypes) {
         activeTab={activeTab}
       />
 
+      <TransferAction
+        measures={measures}
+        onClick={transferMeasure}
+        activeTab={activeTab}
+      />
+
       <AssociateCmsIdAction measures={measures} onClick={associateCmsId} />
+
+      <PipeSeparator />
+
       <VersionAction
         measures={measures}
         onClick={versionMeasure}
@@ -233,20 +251,22 @@ export default function ActionCenter(props: PropTypes) {
         onClick={draftMeasure}
         canEdit={canEdit}
       />
+
+      <PipeSeparator />
+
       <ViewHRAction measures={measures} onClick={viewHumanReadable} />
-      <TransferAction
-        measures={measures}
-        onClick={transferMeasure}
-        activeTab={activeTab}
-      />
       <HistoryAction measures={measures} onClick={viewMeasureHistory} />
       <CompareVersionsAction measures={measures} onClick={compareVersions} />
+
       {featureFlags?.MeasureReviewStatus && (
-        <ReviewAction
-          measures={measures}
-          onClick={reviewMeasure}
-          canEdit={canEdit}
-        />
+        <>
+          <PipeSeparator />
+          <ReviewAction
+            measures={measures}
+            onClick={reviewMeasure}
+            canEdit={canEdit}
+          />
+        </>
       )}
     </div>
   );
