e0949e6b830f24e850e3cfbdfa85eafe557d1579
sb-prateekkeerthi <prateekkeerthi7@gmail.com>
2026-08-18 12:01:13 -0400

    MAT-10185,10191 save manage review data

--- src/types/madie-madie-util.d.ts
+++ src/types/madie-madie-util.d.ts
@@ -349,6 +349,8 @@ declare module "@madie/madie-util" {
     onClose: () => void;
     entityType: "measure" | "library";
     entityId?: string;
+    entitySetId?: string;
+    onSuccess?: () => void | Promise<void>;
   }): React.ReactElement | null;
   export function getNewestMeasureInstance(measures: Measure[]): Measure;
 
