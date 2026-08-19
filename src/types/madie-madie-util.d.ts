d73f546e7782e719234416899a7aa78b6565a22f
nmorasb <nate.moraca@semanticbits.com>
2026-08-19 10:49:16 -0400

    MAT-10298: fix lint issue

--- src/types/madie-madie-util.d.ts
+++ src/types/madie-madie-util.d.ts
@@ -146,7 +146,7 @@ declare module "@madie/madie-util" {
   export class MeasureServiceApi {
     constructor(baseUrl: string, getAccessToken: () => string);
     fetchMeasure(id: string): Promise<Measure>;
-    fetchMeasureBundle(measure: Measure): Promise<Bundle>;
+    fetchMeasureBundle(measure: Measure, bundleType?: string): Promise<Bundle>;
     getMeasuresByMeasureSetId(
       measureSetId: string,
       sortByLatestVersion?: boolean,
@@ -353,7 +353,8 @@ declare module "@madie/madie-util" {
     setToastOpen: (open: boolean) => void,
     setToastType: (type: string) => void,
     setToastMessage: (message: string) => void,
-    elmErrorSeverity: string
+    elmErrorSeverity: string,
+    bundleType?: string
   ): Promise<void>;
   export function downloadZipFile(
     exportData: any,
